3918411b4d9867ebc2c4a353c8dca6b818d0bf5a
kmbarnett <kmbarnett94@gmail.com>
2019-11-15 10:00:21 -0700

    ADD disable button without selection on js eddited index accordingly

--- day_7/my_website/script.js
+++ day_7/my_website/script.js
@@ -1,13 +1,17 @@
 (function() {
-  'use strict';
-let button = document.getElementById("creation-button");
-let heroName = document.getElementById("hero-name");
-let output = document.getElementById("output-div");
-let select = document.getElementById("hero_type");
+  let button = document.getElementById("creation-button");
+  let heroName = document.getElementById("hero-name");
+  let output = document.getElementById("output-div");
+  let select = document.getElementById("hero_type");
+
   function handleClick() {
     event.preventDefault();
     output.innerHTML = `${heroName.value}: ${select.value}`;
   }
-  button.addEventListener("click", handleClick);
 
+  function buttonState() {
+    button.removeAttribute('disabled');
+  }
+  select.addEventListener("change", buttonState);
+  button.addEventListener("click", handleClick);
 }());
