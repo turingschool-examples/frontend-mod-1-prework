822848785b8140675cbb34bf932f3473cb602c98
Casey Dallavalle <cbdallavalle@gmail.com>
2020-03-02 16:19:05 -0700

    Day 1 changes. Updated the Day 2 README and added a new exercise comparisons.js, to help with Comparison Operator comprehension. Started the day 3 README update and created a new exercise decision-making.js to help with If/Else Statement comprehension

---
+++ day_2/exercises/comparisons.js
@@ -0,0 +1,39 @@
+// In the exercises below, write your own code where indicated
+// to achieve the desired result. Run this code using node in terminal.
+
+//TODO: change node description
+
+var firstVar = 10;
+var secondVar = 20;
+var thirdVar = "10";
+
+// example: log the result of the comparison: is firstVar greater than secondVar?
+console.log(firstVar > secondVar); // this should return: false
+
+// log the result of the comparison: is firstVar less than secondVar?
+console.log(); // this should return: true
+
+// log the result of the comparison: is firstVar equal to thirdVar? (use the == operator)
+console.log(); // this should return: true
+
+// log the result of the comparison: is firstVar strictly equal to thirdVar? (use the === operator)
+console.log(); // this should return: false
+
+// note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
+// since the firstVar is an integer value and the thirdVar is a string value, although they are both equal to 10,
+// the comparison still evaluates to false. The equal to operator takes only the value into account.
+
+// log the result of the comparison: is firstVar not equal to secondVar?
+console.log(); // this should return: true
+
+// log the result of the comparison: is secondVar greater than or equal to 20?
+console.log(); // this should return: true
+
+// log the result of the comparison: is secondVar greater than or equal to 21?
+console.log(); // this should return: false
+
+// log the result of the comparison: is secondVar less than or equal to 20?
+console.log(); // this should return: true
+
+// log the result of the comparison: is secondVar less than or equal to 21?
+console.log(); // this should return: true
---
+++ day_3/exercises/decision-making.js
@@ -0,0 +1,56 @@
+// In the exercises below, write your own code where indicated
+// to achieve the desired result. Run this code using node in terminal.
+
+//TODO: change node description
+
+// If/Else Statements are expressions your program uses evaluate to different outcomes.
+// Below is a dynamic story that is created based on the value of three variables: doorChoice, bearClothing, bearChoice.
+// Spend some time changing the variables and running the file to see how the story changes.
+
+var doorChoice = 1;
+var bearClothing = "";
+var bearChoice = 1;
+
+console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
+
+if (doorChoice === 1) {
+    bearClothing = "hat";
+}
+else {
+    bearClothing = "scarf";
+}
+
+console.log("You see a bear putting on a " + bearClothing);
+console.log("It looks like that " + bearClothing + " is too small for the bear, do you...");
+
+console.log("1. Offer your own to the bear?");
+console.log("2. Point it out to the bear?");
+console.log("3. Make a dash for the next room?");
+
+if (bearChoice === 1) {
+    console.log("You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!");
+}
+else if (bearChoice === 2) {
+    console.log("You tell the bear the " + bearClothing + " is too small and it starts to cry!");
+}
+else if (bearClothing === 3) {
+    console.log("You run as fast as you can into the next room. It's full of snakes!");
+}
+else {
+    console.log("You stay with the bear and become it's best friend!");
+}
+
+// Reflection Questions
+// 1. In English, using technical vocabulary, describe what is happening between lines 17 and 22.
+
+// 2. What variable has a new value assigned to it after the first if statement executes?
+
+// 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+
+// 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 42.
+
+// 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+
+// 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+
+// 7. What is your favorite ending?
