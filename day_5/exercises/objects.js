70cb42eb77df1787eb8c719f3aa4003324814a22
Casey Dallavalle <cbdallavalle@gmail.com>
2020-03-04 13:56:24 -0700

    Add day 5 README. Added materials, questions, and exercises for object comprehension

---
+++ day_5/exercises/objects.js
@@ -0,0 +1,32 @@
+/*
+In the exercises below, write your own code where indicated
+to achieve the desired result. Run this code using node in terminal.
+
+//TODO: change node description
+*/
+
+// example: Write code below to print an object that holds grocery store inventory
+var foods = {
+    apples: 23, 
+    grapes: 507, 
+    eggs: 48
+};
+console.log(foods);
+
+// Write code below that will print an object of animals and their number
+// at the zoo. (an inventory of animals)
+var zoo = //YOUR CODE HERE
+console.log(zoo)
+
+// Using the zoo that you created above, print all the keys in the object.
+// YOUR CODE HERE
+
+// Using the zoo that you created above, print all the values in the object.
+// YOUR CODE here
+
+// Using the zoo taht you created above, print the value of the first item in
+// the object
+// YOUR CODE HERE
+
+// Add an animal to the zoo object and print the updated object.
+// YOUR CODE HERE
