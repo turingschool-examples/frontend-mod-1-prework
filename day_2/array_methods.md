8291e1e64b0abbad0c4695641435dcb67c1048c2
jobbot <joevarela83@gmail.com>
2020-03-29 11:43:03 -0600

    Add day 2 work

---
+++ day_2/array_methods.md
@@ -0,0 +1,34 @@
+## Array Methods
+
+1. **Pop**: This method removes the last item listed in the arrays index.
+
+```
+var veggies = ["Peas", "Carrots", "Onions"];
+var last = fruits.pop();
+console.log(veggies);
+["Pease", "Carrots"]
+```
+
+1. **Push**: This method adds a new item to the last position in the array index.
+
+```
+var newLength = veggies.push("Cabbage");
+console.log(veggies);
+["Peas", "Carrot", "Cabbage"]
+```
+
+1. **Shift**: Similar to the `pop` method, the `shift` method removes the first item in the array index.
+
+```
+var first = veggies.shift();
+console.log(veggies);
+["Carrots", "Cabbage"]
+```
+
+1. **Unshift**: Adds an item to the first index in an array.
+
+```
+var newLength = veggies.unshift("Squash");
+console.log(veggies);
+["Carrots", "Cabbage", "Squash"]
+```
--- day_2/exercises/array.js
+++ day_2/exercises/array.js
@@ -10,7 +10,8 @@ var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals);
 
 // Write code below that will log an array of foods. Store the array in a variable.
-// YOUR CODE HERE
+var foods = ["Milk", "Cheese", "Dairy"];
+console.log(foods);
 
 // example: Write code below that will log the number of elements in array of
 // animals from above.
@@ -18,28 +19,31 @@ console.log(animals.length);
 
 // Write code below that will log the number of elements in the array of
 // foods from above.
-// YOUR CODE HERE
+console.log(foods.length);
 
 // Write code below that will log "Zebra" from the animals array
-// YOUR CODE HERE
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the first item in the animals array
-// YOUR CODE HERE
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the last item in the animals array
-// YOUR CODE HERE
+console.log(animals[2]);
 
 // Write code using bracket notation that will reassign the last item in the animals
 // array to "Gorilla"
-// YOUR CODE HERE
+var newLength = animals.push("Gorilla");
+console.log(animals);
 
 // Write code below that will log the last item from the foods array.
-// YOUR CODE HERE
+console.log(foods[2]);
 
 // Write code below that uses a method to add "lion" to the animals array and
 // log the result
-// YOUR CODE HERE
+var first = animals.unshift("lion");
+console.log(animals[0]);
 
 // Write code below that removes the last item of food from the foods array and
 // log the result
-// YOUR CODE HERE
+var last = foods.pop();
+console.log(foods);
