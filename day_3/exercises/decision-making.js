7ee6875b4c14b2c1c38dd87bf00b4722b6da3af5
Benjamin Brown <benjamin.moses.brown@gmail.com>
2020-03-31 19:53:00 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -41,15 +41,41 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+Lines 17 through 21 are an if/else statement which determines which code to run
+depending on the door selection. If the door choice is exactly 1, the variable
+"bearClothing" will return as "hat". Any other selection but 1 will return as
+"scarf" instead.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+"bearClothing"
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+"scarf"
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+
+These lines are an if/else if/if statement that determines which of four
+possible lines will be logged to the console. The values 1 and 2
+return unique lines. Any other selection returns the fourth line. The third line
+"You run as fast as you can into the next room. It's full of snakes!" will not
+run as there is a (likely) error where the "bearChoice" variable has been
+substituted for "bearClothing".
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+The outcome will be the else condition "You stay with the bear and become it's
+best friend!" although the intent of the coder was likely to return the line
+"You run as fast as you can into the next room. It's full of snakes!"
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+"You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+
+I would give the bear my scarf because I don't wear scarves and I don't want to
+be stuck forever with this emotionally unstable bear.
+
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,37 @@
 
 1. What is a conditional statement? Give three examples.
 
+A conditional statement asks a true or false question and then gives returns a
+certain output based on the answer.
+
+If it's hot, wear a t-shirt.
+If else it's warm, wear a long sleeve shirt.
+Else, wear a coat.
+
 1. Why might you want to use an if-statement?
+
+To create branches in your code.
 
 1. What is the JavaScript syntax for an if statement?
 
+if (var = "value") {
+  console.log("return value")
+}
+
 1. How do you add multiple conditions to an if statement?
+
+Incorporate "if", "else if", and "else".
 
 1. What is the JavaScript syntax for an if/else if/else statement?
 
+if (var = "value") {
+  console.log("return") {
+  else if (var = "other value");
+  console.log("other return");
+  } else {
+    console.log("other other return");
+}
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+No. Are there other kinds of conditionals than if-statements?
