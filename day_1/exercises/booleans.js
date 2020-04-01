d58c4448499cf074dde5304b484a558e4ceff41f
Taryn Martin <taryn.martin919@gmail.com>
2020-04-01 17:20:45 -0600

    Add day 1

--- day_1/exercises/booleans.js
+++ day_1/exercises/booleans.js
@@ -5,7 +5,7 @@ file from your terminal with the command `node day_3/exercises/comparisons.js`
 */
 
 // example: log to the console the data type of `true`:
-console.log(typeof true);
+console.log(typeof (true));
 
 // example: log to the console the data type of 1 is equal to 1:
 console.log(typeof (1 == 1));
@@ -14,4 +14,4 @@ console.log(typeof (1 == 1));
 console.log(1 == 2);
 
 // example: log to the console the result of 7 is  not equal to 2:
-console.log(1 == 2);
+console.log(7 != 2);
