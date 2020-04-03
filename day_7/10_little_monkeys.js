804c1bf174db5b80f0e8f769de5ea57ce22cea63
Nick Hart <nick.hart.des@gmail.com>
2020-04-03 10:39:48 -0600

    Add 10 little monkeys exercise

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,35 @@
+function getCount(number) {
+  var monkeyCount = number
+}
+
+var text = ' little monkeys jumping on the bed, one fell off and bumped his head, Mama called the doctor and the doctor said, "No more monkeys jumping on the bed!"';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+> Three little monkeys jumping on the bed,
+> One fell off and bumped his head,
+> Mama called the doctor and the doctor said,
+> "No more monkeys jumping on the bed!"
+>
+> Two little monkeys jumping on the bed,
+> One fell off and bumped his head,
+> Mama called the doctor and the doctor said,
+> "No more monkeys jumping on the bed!"
+>
+> One little monkey jumping on the bed,
+> He fell off and bumped his head,
+> Mama called the doctor and the doctor said,
+> "Get those monkeys right to bed!"
+*/
