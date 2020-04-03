b8e134b42e17c7afe5df7652487a360e30aa2d9b
Nick Hart <nick.hart.des@gmail.com>
2020-04-03 09:48:01 -0600

    Add person exercise

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,4 +8,38 @@ person methods below the class so that they print their result to the
 terminal.
 */
 
-// YOUR CODE HERE
+class Person {
+  constructor(name, age, isTired) {
+    this.name = name;
+    this.age = age;
+    this.isTired = isTired;
+  }
+
+  sleep() {
+    this.isTired = false;
+  }
+
+  exercise() {
+    this.isTired = true;
+  }
+
+  celebrateBirthday() {
+    return this.age += 1;
+  }
+}
+
+var nick = new Person("Nick", 23, true);
+
+console.log(nick);
+
+nick.sleep();
+
+console.log(nick);
+
+nick.exercise();
+
+console.log(nick);
+
+nick.celebrateBirthday();
+
+console.log(nick);
