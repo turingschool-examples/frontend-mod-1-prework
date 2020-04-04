2f51e17548b1e16acfb879be4a1418dcbd3c79bc
Brigette Doelp <brigette.doelp@gmail.com>
2020-04-04 15:41:39 -0700

    Answer questions and exercises

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -14,17 +14,31 @@ printName();
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName() {
+  console.log("Brigette Doelp")
 };
 
-printName("Albus Dumbledore");
+printName();
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+function add(one, two) {
+  return one + two;
+}
+add (6, 7)
+
+//literally spent a half hour on this and overcomplicated the poop outta it till i
+//remembered i'm literally only adding step back you're not splitting the atom AAAAAHHH nailed it.
+
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+
+function mollySolverson(fargoOne, fargoTwo) {
+  var fargoOne = "All that";
+  var fargoTwo = "for a little bit of money";
+  console.log(fargoOne.concat(" ", fargoTwo));
+}
+
+mollySolverson (fargoOne, fargoTwo)
