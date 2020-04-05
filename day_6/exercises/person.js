5bb7500b5786dd8b5ee108809792eb9d8e3b26df
Benjamin Brown <benjamin.moses.brown@gmail.com>
2020-04-05 17:26:30 -0600

    Add Day 6 Work

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,4 +8,26 @@ person methods below the class so that they print their result to the
 terminal.
 */
 
-// YOUR CODE HERE
+class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.happy = false;
+  }
+  wish_happy_birthday() {
+    console.log("Happy Birthday, " + this.name + "!" + " You're " + this.age + ".");
+  }
+  throw_party() {
+    this.happy = true;
+  }
+}
+
+var tom = new Person("Tom", 22);
+
+console.log(tom);
+
+tom.wish_happy_birthday();
+
+tom.throw_party();
+
+console.log(tom);
