29880bc41059d92715ccda4ced8968fd952d3602
Ben McClung <aurumvalian@gmail.com>
2020-04-05 16:08:40 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -8,9 +8,9 @@ Below is a dynamic story that is created based on the value of three variables: 
 Spend some time changing the variables and running the file to see how the story changes.
 */
 
-var doorChoice = 1;
+var doorChoice = 2;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 0;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -34,22 +34,28 @@ if (bearChoice === 1) {
 } else if (bearClothing === 3) {
   console.log("You run as fast as you can into the next room. It's full of snakes!");
 } else {
-  console.log("You stay with the bear and become it's best friend!");
+  console.log("You stay with the bear and become its best friend!");
 }
 
 /*
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
-
+ If the variable doorChoice is strictly equal to 1, hat will be assigned to the bearClothing variable. If it is not,
+ scarf will be assigned to the bearClothing variable.
 2. What variable has a new value assigned to it after the first if statement executes?
-
+bearClothing
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
-
+scarf
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
-
+If the variable bearChoice is strictly equal to 1, the string starting with "You offer the bear" will be logged to the console.
+If it is not strictly equal to 1 but is strictly equal to 2, the string starting with "You tell the bear" will
+be logged to the console.
+If it is neither strictly equal to 1 or 2 but is strictly equal to 3, the string starting with "You run as fast" will be
+logged to the console.
+If none of those are true, the string starting with "You stay with the bear" will be logged to the console.
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
-
+"You run as fast as you can into the room. It's full of snakes!"
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-
+"You tell the bear the hat is too small and it starts to cry!"
 7. What is your favorite ending?
-*/
+I'm a personal fan of the fourth ending, as I like bears. */
