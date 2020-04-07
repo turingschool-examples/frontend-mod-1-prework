3dc2bc12c3fc09c526123d08537bfd2b6ab5c67c
Tyler Haglund <relyt4me@gmail.com>
2020-04-07 01:32:18 -0600

    Add Day 2 Work

---
+++ day_2/array_methods.md
@@ -0,0 +1,12 @@
+# Array Methods #  
+## Pop ##  
+   The pop method removes the last element in an array. It is written like this:
+   `list.pop();`  
+
+## Push ##  
+   A push method adds a new element to the array at the end. It is written like this: `list.push('new item');`  
+
+## Shift ##  
+   Shift also removes and element from an array but unlike the pop method this removes the first element. It is written like this: `list.shift();`
+## Unshift ##  
+   Unshift allows you to add an element to the front of the array increasing its length. It is written like this: `list.unshift('new item');`
--- day_2/exercises/iteration.js
+++ day_2/exercises/iteration.js
@@ -16,13 +16,20 @@ for (var i = 0; i < animals.length; i++) {
 // <animal> is awesome!" for each animal.
 
 for (var i = 0; i < animals.length; i++) {
-  // YOUR CODE HERE
+  console.log("The " + animals[i] + " is awesome!");// YOUR CODE HERE
 }
 
 // Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
-// YOUR CODE HERE
+var foods = ["Milk", "Eggs", "Bread", "Cereal", "Sugar"];
+
+for (var i = 0; i < foods.length; i++) {
+  console.log("Add " + foods[i] + " to the shopping list");
+}// YOUR CODE HERE
 
 // Create an array of numbers and then iterate over that array to log doubles
 // of each of the number.
-// YOUR CODE HERE
+var numbers = [3, 7, 2, 4, 9]
+for (var i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] + "" + numbers[i]);
+}// YOUR CODE HERE
