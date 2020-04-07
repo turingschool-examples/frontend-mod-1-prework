bbfed7f82d03291fdb1d503c9e8767f195f6eaf3
Nick Hart <nick.hart.des@gmail.com>
2020-04-07 12:50:58 -0600

    Update high level explanation and caeser cipher code

--- day_7/caeser_cipher.js
+++ day_7/caeser_cipher.js
@@ -1,38 +1,27 @@
-var alphabet = {
-  1: 'A',
-  2: 'B',
-  3: 'C',
-  4: 'D',
-  5: 'E',
-  6: 'F',
-  7: 'G',
-  8: 'H',
-  9: 'I',
-  10: 'J',
-  11: 'K',
-  12: 'L',
-  13: 'M',
-  14: 'N',
-  15: 'O',
-  16: 'P',
-  17: 'Q',
-  18: 'R',
-  19: 'S',
-  20: 'T',
-  21: 'U',
-  22: 'V',
-  23: 'W',
-  24: 'X',
-  25: 'Y',
-  26: 'Z'
+function caeserCipher(phrase, key) {
+  var upperPhrase = phrase.toUpperCase(); // to make it uniform, make all characters uppercase
+
+  var newPhraseArr = []; // create an empty array to store new characters
+
+  for (i = 0; i < upperPhrase.length; i++) { // for the length of the phrase input...
+    if (upperPhrase[i] == ' ' || upperPhrase[i] == '!' || upperPhrase[i] == '.' || upperPhrase[i] == ',' || upperPhrase[i] == '"' || upperPhrase[i] == '?' || upperPhrase[i] == '\'') { // IF the character is punctuation/space...
+      newPhraseArr.push(upperPhrase[i]); // just ignore it, add it to the array as is
+    } else { // if the character isn't a space or punctuation though...
+      var newCharCode = upperPhrase.charCodeAt(i) + key; // get ASCII code for character and add the key to it
+      if (newCharCode > 90) { // if the new ASCII code goes past the value for "Z"...
+        var overflow = (newCharCode % 65 - 26) + 65; // formula that wraps it back around to ASCII value between 65 and 90, inclusive
+        var overflowChar = String.fromCharCode(overflow); // converts the new ASCII value back to a character
+        newPhraseArr.push(overflowChar); // adds the character to the array
+      } else {
+        var newChar = String.fromCharCode(newCharCode); // convert new ASCII code to new character
+        newPhraseArr.push(newChar); // add new character to the array
+      }
+    }
+  }
+
+  var newPhrase = newPhraseArr.join(''); // convert the array of new characters to a string
+
+  console.log(newPhrase); // return the new string
 }
 
-function caeserCipher(phrase, key) {
-  var charArray = Array.from(phrase);
-
-  for (i = 0; i < alphabet.length; i++) {
-    if (charArray[i] == alphabet[i]) {
-      charArray[i] = alphabet[i];
-    }
-    console.log(charArray);
-  }
+caeserCipher("hello there", 12);
--- day_7/high_level.md
+++ day_7/high_level.md
@@ -1,28 +1,14 @@
 ### The Caeser Cipher
-###### So the goal is to create a function that takes a string input and an integer for how many letters to offset each letter in the string, and then outputs a new "ciphered" string.  
+##### So the goal is to create a function that takes a string input and an integer for how many letters to offset each letter in the string, and then outputs a new "ciphered" string.  
 ---
-I would need to start by assigning each letter in the alphabet a number value, so I would create an "alphabet" object doing exactly that, with the letters as the keys and the numbers as the values.  
-```
-var alphabet = {
-  'A': 1,
-  'B': 2,
-  'C': 3,
-  'D': 4,
-  'E': 5,
-  'F': 6,
-  // and so on
-}
-```
-Then, I would declare the function and the arguments it takes, one being the string and the next being the number to offset each letter by.
-```
-function caeserCipher(phrase, key) {
-  // code
-}
-```
-Within the function, I'd implement a series of steps:
-1. Use a method to convert the `phrase` argument from a string to an array of characters
-2. Using the `alphabet` object as a reference, output a new array of the number value of the characters
-3. Add the `key` value to each value in the array, output a new array with updated values
-4. Referencing the `alphabet` object, output a new array with letters that match the corresponding updated values
-5. Use another method to convert the new array back to a string
-6. Print the new string
+From prior experience, I'm familiar with ASCII codes that correspond with each character. With this in mind, I created a function that followed these steps:
+
+1. Create a loop that iterates for the length of the phrase input
+2. Tell that loop to ignore punctuations and spaces
+3. Convert a character in the phrase to its ASCII code
+4. Add the key to the ASCII code, and add a formula that "wraps around" back to 65 (the ASCII for capital A) + the key if needed
+5. Convert the new ASCII code back to its respective letter
+6. Add that letter to a new array
+7. Repeat 3 - 6 for each character in the phrase
+8. Convert the array to a string
+9. And then print the new string! 
