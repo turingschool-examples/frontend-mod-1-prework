86a3232e450d24ac4988ddf15072696890c63301
Brigette Doelp <brigette.doelp@gmail.com>
2020-04-07 18:35:12 -0700

    Write project code

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,9 @@
+function tenLittleMonkeys(allMonkeys, oneLessMonkey, twoLessMonkeys) {
+  var allMonkeys = "Ten"
+  var oneLessMonkey = "Nine"
+  var twoLessMonkeys = "Eight"
+  console.log(allMonkeys.concat(" little monkeys jumping on the bed, One fell off and bumped his head, Mama called the doctor and the doctor said, 'No more monkeys jumping on the bed!'"));
+  console.log(oneLessMonkey.concat(" little monkeys jumping on the bed, One fell off and bumped his head, Mama called the doctor and the doctor said, No more monkeys jumping on the bed!"));
+  console.log(twoLessMonkeys.concat(" little monkeys jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, 'Get those monkeys right to bed!'"));
+
+}
