d193e46f8ab401f3981c27efc46c8ff0f7c649b0
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-08 12:19:11 -0600

    Initial commit

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -40,16 +40,30 @@ if (bearChoice === 1) {
 /*
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
+An if/else conditional statement is being used to assign a new value to the variable bearClothing.
+To decide what the value of `var bearClothing’ should be, the **if** statement asks “does the condition in parentheses evaluate to truthy”? (i.e. is the condition met). If yes, the code in the if statement will execute, assigning ‘hat’ to the variable bearClothing: `var bearClothing = ‘hat’;`
+
+If the **if** statement is executed the `else` statement will be disregarded (skipped over) as it's now redundant.
+
+If the condition in the `if` statement evaluates to falsy (i.e. the value of `var doorChoice` is not identical to `1`) then the code in the `else` statement will execute, assigning  ‘scarf’ to the variable bearClothing: `var bearClothing = ‘scarf’;`
 
 2. What variable has a new value assigned to it after the first if statement executes?
+bearClothing has a new variable assigned to it after the first if statement executes.
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+Scarf - because if the value of the variable doorChoice is not identical to 1 the else statement will execute.
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+This code block is testing multiple conditions in order to decide which string should be logged. Once a condition is met (evaluated as truthy), the code in the statement will be executed, and the following conditional statements in the code block will be disregarded (won’t be tested).
+If none of the **if/else if** conditions are met, the `else` statement will execute by default as it does not have any conditions associated with it.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+Answer with current code: "You stay with the bear and become it's best friend!"
+**WHEN VARIABLE IS UPDATED TO (bearChoice === 3) IT WILL BE:** "You run as fast as you can into the next room. It's full of snakes!"
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+"You tell the bear the hat is too small and it starts to cry!"
 
 7. What is your favorite ending?
+"You stay with the bear and become it's best friend!"
 */
