760ef2d2a2f7ef9574cb24e2e0f9e7520f342c90
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-04-08 21:15:58 -0600

    Add day 3 work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -41,15 +41,36 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+Line 17 evaluates if you have chosen door 1.
+If true, then line 18 tells us that the bear clothing will be a hat. And it doesn't need to read lines 19 - 21.
+If it's false, meaning you didn't choose door 1, then it keeps reading.
+And in line 20 we find out the bear clothing will be a scarf.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+From the text in line 15, it seems as though your only options for doorChoice are 1 or 2, "You enter a dark room with two doors. Do you go through #1 or #2?".
+But if you are able to set the variable doorChoice to 3, than the bearClothing value would be "scarf" because you did not choose door 1.
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+
+Line 30 asks if you chose option 1. If true, then the console will log line 31 and then skip lines 32 - 38.
+If your bearChoice was not 1, then it will keep reading. Line 32 asks if you chose option 2. If true, then the console will log line 33 and then skip lines 34 - 38.
+If your bearChoice was not 2, then it will keep reading. Line 34 asks if you chose option 3. If true, then the console will log line 35 and then skip lines 36 -38.
+If your bearChoice was not 3, then it will keep reading. Line 36 asks if you did something else which will always be true if you've gotten to this point and answer to the previous questions were false. The console will log line 37.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+You run as fast as you can into the next room. It's full of snakes!
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+You tell the bear the hat is too small and it starts to cry!
+
 7. What is your favorite ending?
+
+You offer the bear your scarf and the bear shows you a secret passage out! This is my favorite because I have way too many scarfs and I'd like to see a bear wear one of mine.
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,52 @@
 
 1. What is a conditional statement? Give three examples.
 
+A conditional statement says what to do in a given situation
+
+```JavaScript
+if (score > 100) {
+  console.log("You win!");
+}
+
+if (onVaction == true) {
+  console.log("I will get back to you next week.");
+}
+
+if (dessert == "cookie") {
+  console.log("Mouse wants a glass of milk.");
+}
+```
+
 1. Why might you want to use an if-statement?
+
+You'll want to use an if-statement when there are different things that will happen dependent upon some condition.
 
 1. What is the JavaScript syntax for an if statement?
 
+```JavaScript
+if (conditional statement) {
+  execute();
+}
+```
+
 1. How do you add multiple conditions to an if statement?
+
+You can add multiple conditions with logical operators. The logical and operator, &&, will evaluate to true if and only if both expressions are true. The logical or operator, ||, will evaluate to true if either or both expressions are true.
 
 1. What is the JavaScript syntax for an if/else if/else statement?
 
+```JavaScript
+if (conditional statement) {
+  execute();
+} else if (conditional statement) {
+  execute();
+} else if (conditional statement) {
+  execute();
+} else {
+  execute();
+}
+```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+At this point, I think of the terms if-statement and conditional statements as being interchangeable. But I believe conditional statements can also be used in switch statements which match a value to a case.
