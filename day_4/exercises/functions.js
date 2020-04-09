e478475af33644c46ffe7d617b08a69b8525a5e5
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-04-09 12:39:07 -0500

    these are my day_4 answers

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,32 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
+/*function printName() {
   console.log("Severus Snape");
 };
 
 printName();
-
+*/
 // Update the function below so that it takes an argument of your name and
 // prints your name
-
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+/*
+function printName() {
+  console.log("Jeff Woltjen");
+  printName()
 };
-
-printName("Albus Dumbledore");
-
+*/
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
 
+function addNumber(number1, number2) {
+  console.log (number1 + number2);
+}
+var sum = addNumber(3,4)
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
 // YOUR CODE HERE
+function sayHello(firstName, secondName) {
+  console.log ("Hello " + firstName + " Hello " + secondName);
+}
+var greeting = sayHello("hodor", "bran");
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,15 +1,29 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a function?
+a function runs a process based upon the parameters that are inputted.
 
-1. What is a parameter? 
+1. What is a parameter?
+Parameters are variables within the function expression.
 
 1. What is a return value?
+A return value is the value that the function spits out after running.
 
 1. In the space below, create a function named `hello` that will print `"Sam I am"`.
+function sayHello(){
+  console.log("Sam I am");
+}
+sayHello();
 
 1. Create a function name `hello_someone` that takes an argument of `name` and logs `name + " I am"`.
+// I'm using camelCase because it is the preferred JavaScript convention.
 
+function helloSomeone(name){
+  console.log(name + " I am");
+  var name = hodor
+}
 1. How would you call or execute the function that you created above?
+helloSomeone(); 
 
 1. What questions do you still have about functions in Javascript?
+I'm unclear on the difference between functions and function expressions.
