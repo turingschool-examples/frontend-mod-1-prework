108425bb7290aa1714029590467befa8796603c5
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-04-10 10:42:23 -0600

    Add Day 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,59 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
-  console.log("Severus Snape");
-};
+var myName = "Nicole Latifi"
 
-printName();
+function printName1() {
+  console.log("My name is " + myName);
+}
+
+printName1();
+
+//Or
+
+function printName2() {
+  console.log("Nicole");
+}
+
+printName2();
+
+//Or
+
+function printName3() {
+  var myName = "Nicole L";
+  console.log(myName)
+}
+
+printName3();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName4(firstName, lastName) {
+  var firstName = "Nicole";
+  var lastName = "Latifi";
+  console.log(firstName + lastName)
 };
 
-printName("Albus Dumbledore");
+printName4();
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+function sumTwoNumbers(firstNumber, secondNumber) {
+  var firstNumber = 2;
+  var secondNumber = 5;
+  console.log(firstNumber + secondNumber)
+};
+
+sumTwoNumbers();
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+function cocktailGenerator(alcohol, mixer) {
+  var alcohol = "rum";
+  var mixer = "coke";
+  console.log("I'll have a " + alcohol + " and " + mixer + ", please!")
+};
+
+cocktailGenerator();
--- day_4/questions.md
+++ day_4/questions.md
@@ -2,14 +2,39 @@
 
 1. In your own words, what is the purpose of a function?
 
-1. What is a parameter? 
+A function organizes a specific command or a group of commands so that you can later call that function whenever needed.
+
+1. What is a parameter?
+
+A parameter is a piece of information needed in a function. It is a value that will be inputted into the function.
 
 1. What is a return value?
 
+The return value is the response of the function once it is called.
+
 1. In the space below, create a function named `hello` that will print `"Sam I am"`.
+```JavaScript
+function hello() {
+  var helloMessage = "Sam I am";
+  console.log(helloMessage);
+}
+
+hello();
+```
 
 1. Create a function name `hello_someone` that takes an argument of `name` and logs `name + " I am"`.
+```JAVASCRIPT
+function hello_somone() {
+  var name = ;
+  console.log(name + " I am");
+}
+```
 
 1. How would you call or execute the function that you created above?
+```JavaScript
+hello_someone();
+```
 
 1. What questions do you still have about functions in Javascript?
+
+There are a few different ways to write a function, i.e. with the variable can be inside or outside of the function. And I'm wondering if one way is the conventional method or better in some way.
