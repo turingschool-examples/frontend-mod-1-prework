5653f36ced2101b979e0fae3455f5e1c53505cfa
Hanna Kim <hannakim91@gmail.com>
2020-04-10 19:30:18 -0600

    Add Day 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -6,25 +6,38 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 
 //example: Write a function below that, when called will print your name
 function printName() {
-  console.log("Severus Snape");
-};
+  console.log("Hanna Kim");
+}
 
 printName();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
-};
+function myName(name) {
+  console.log(name);
+}
 
-printName("Albus Dumbledore");
+myName("Hanna Kim");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+
+function addNumbers(a, b) {
+  return a + b;
+}
+console.log(addNumbers(1, 2));
+console.log(addNumbers(2, 3));
+
+
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
 // YOUR CODE HERE
+
+function knockKnock(fruitOne, fruitTwo) {
+  console.log(fruitOne + " you glad I didn't say " + fruitTwo);
+}
+
+knockKnock("orange", "banana");
