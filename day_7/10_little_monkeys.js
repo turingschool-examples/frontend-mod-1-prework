5a66dff3ea92c1818ad3a75c0f5e8d0c28dc263f
Ben McClung <aurumvalian@gmail.com>
2020-04-11 12:10:23 -0600

    Added Projects

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,7 @@
+var numArray = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
+for ( var i = 0; i < 10; i++) {
+  if (i < 9) {
+    console.log(numArray[i] + ' little monkeys jumping on the bed, \n One fell off and bumped his head, \n Mama called the doctor and the doctor said, \n "No more monkeys jumping on the bed!" \n');
+  } else (
+    console.log('One little monkey jumping on the bed, \n He fell off and bumped his head, \n Mama called the doctor and the doctor said, \n "Get those monkeys right to bed!"')
+) };
---
+++ day_7/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(num) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
+    console.log("Fizz");
+  } else if (num % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  };
+}
+
+for (var i = 1; i < 101; i++) {
+  fizzBuzz(i);
+}
