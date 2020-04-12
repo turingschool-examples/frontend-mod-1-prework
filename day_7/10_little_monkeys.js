d911b0aaaecb82e012363d4659cc5fd4ebc4b992
Rachel Williams <rachelwilliams659@gmail.com>
2020-04-12 14:53:57 -0600

    Add function version of 10 little monkeys

--- day_7/10_little_monkeys.js
+++ day_7/10_little_monkeys.js
@@ -16,5 +16,23 @@ for (i = 10; i>0; i--) {
   }
 }
 
-//For the bonus: I don't understand how you would write this program to work with 
-//any number of monkeys without storing an infite number of values in your array.
+// This code runs for a different number of monkeys ONLY if you feed in a different
+// stringArray with the string values of the numbers going up to whatever
+// number you inputted.I think you would need a string of infinite values to
+// get this work for any arbitrary number. 
+function monkeyRhyme(monkeyNumber){
+  for (i = monkeyNumber; i>0; i--) {
+    if (i > 1) {
+      console.log(stringArray[i - 1] + ' little monkeys jumping on the bed,');
+      console.log('One fell off and bumped his head,');
+      console.log('Mama called the doctor and the doctor said,');
+      console.log('"No more monkeys jumping on the bed!"' + '\n');
+      }
+    else if (i == 1) {
+      console.log('One little monkey jumping on the bed,');
+      console.log('He fell off and bumped his head,');
+      console.log('Mama called the doctor and the doctor said,')
+      console.log('"Get those monkeys right to bed!"');
+    }
+  }
+}
