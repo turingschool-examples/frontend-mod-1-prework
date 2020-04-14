b6c7901e23ef7e4aa22bdc9bb7ba9d6d5646d61a
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-14 13:29:39 -0600

    Delete instructions that were copied over to file for reference

--- day_7/10_little_monkeys.js
+++ day_7/10_little_monkeys.js
@@ -1,25 +1,3 @@
-/* Create a file named `10_little_monkeys.js` and within that file, write a program that will print the following nursery rhyme, but for *10* monkeys.
-
-> Three little monkeys jumping on the bed,
-> One fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "No more monkeys jumping on the bed!"
->
-> Two little monkeys jumping on the bed,
-> One fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "No more monkeys jumping on the bed!"
->
-> One little monkey jumping on the bed,
-> He fell off and bumped his head,
-> Mama called the doctor and the doctor said,
-> "Get those monkeys right to bed!"
-
-### Bonus
-Can you write the program so that it will run for any number of monkeys?
-*/
-
-
 var monkeys = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
 
 for (i = 9; i >=0; i--) {
