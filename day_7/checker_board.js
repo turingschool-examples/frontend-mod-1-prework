fb65c0f97abdddd510325db2b0186f87f5bee248
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-14 13:12:24 -0600

    Edit checker board code with final solution

--- day_7/checker_board.js
+++ day_7/checker_board.js
@@ -1,24 +1,12 @@
-for (var i = 1; i <= 6; i++) {
-  if (i % 2 === 0){
-    console.log(" X X X");
-  }else {
-    console.log("X X X ");
+function checkerBoard(size) {
+  for (var i = 1; i <= size; i++) {
+    if (i % 2 === 0){
+      console.log(' X X X');
+    } else {
+      console.log('X X X ');
+    }
   }
-}; // works and can adjust condition to change size of it.
-
-
-function drawBoxesLine1() {
-    var b = 'X';
-    var w = ' ';
-console.log(b, w, b, w, b, w);
-} function drawBoxesLine2() {
-    var b = 'X';
-    var w = ' ';
-console.log(w, b, w, b, w, b);
 }
 
-
-function drawBoxesLine1() {
-    var b = 'X';
-    var w = ' ';
-console.log(b, w, b, w, b, w, "\n", w, b, w, b, w, b);
+// User input in parentheses() to determine size of board:
+checkerBoard(8);
