91b2182513076d86a510ee4af5a7a7a3062038c6
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-14 13:40:48 -0600

    Edit format, add line breaks to strings

--- day_7/10_little_monkeys.js
+++ day_7/10_little_monkeys.js
@@ -2,8 +2,8 @@ var monkeys = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', '
 
 for (i = 9; i >=0; i--) {
   if (i > 0) {
-    console.log(`${monkeys[i]} little monkeys jumping on the bed, One fell off and bumped his head, Mama called the doctor and the doctor said, \"No more monkeys jumping on the bed!\"`);
+    console.log(` ${monkeys[i]} little monkeys jumping on the bed,\n One fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"No more monkeys jumping on the bed!\" \n`);
   } else {
-    console.log(`${monkeys[i]} little monkey jumping on the bed, He fell off and bumped his head, Mama called the doctor and the doctor said, \"Get those monkeys right to bed!\"`);
+    console.log(` ${monkeys[i]} little monkey jumping on the bed,\n He fell off and bumped his head,\n Mama called the doctor and the doctor said,\n \"Get those monkeys right to bed!\"`);
   }
 }
