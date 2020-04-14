f7939e50098dcf3dedf4e76db03f0e703458126a
chad-dickerson <chad.alan.dickerson@gmail.com>
2020-04-14 15:22:57 -0600

    Added 10_little_monkeys.js and fizzbuzz.js

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,8 @@
+var numbers = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+var one = "One"
+
+for (var i = 0; i < numbers.length; i++)
+   console.log(numbers[i] + ' little monkeys jumping on the bed, \nOne fell off and bumped his head, \nMama called the doctor and the doctor said,\n"No more monkeys jumping on the bed!"')
+   console.log(one + ' little monkey jumping on the bed, \nHe fell off and bumped his head, \nMama called the doctor and the doctor said, \n"Get those monkeys right to bed!"')
+
+//Bonus: Yes as long as I add more values to the numbers array. For my program it would be more time consumptive to do so.
--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -1,14 +1,13 @@
-//var fizzbuzz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ,25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-for (var i = 0; i < 100; i++) {
-  if (i % 3) {
-  return ("Fizz")
+for (var i=1; i <= 100; i++)
+{
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else
+        console.log(i);
 }
-else if (i % 5) {
- return ("Buzz")
-}
-else if (i % (3 && 5)) {
-  return ("Fizzbuzz")
-}
-}
-while (i++ < 100)
-console.log(i)
+//Bonus: All you need to do, in order to use the program on any range of numbers is change the values in the following underscored spots.
+// for (var i=__; i<= __; i++)
