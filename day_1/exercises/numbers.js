2702480256478b0c884227afdc5558508dcce93a
Cameron Romo <cromo1225@gmail.com>
2020-04-15 16:20:19 -0600

    Added numbers, whoops

--- day_1/exercises/numbers.js
+++ day_1/exercises/numbers.js
@@ -6,14 +6,16 @@ file from your terminal with the command `node day_3/exercises/comparisons.js`
 
 // example: log the result of the sum of 2 and 2 to the console:
 console.log(2 + 2);
-
-
+4
 // log the result of 7 subtracted from 83 console:
 console.log(83 - 7);
-
+76
 // log the result of 6 multiplied by 53 to the console:
 console.log(6 * 53);
+318
 // log the result of 20 divided by 4 to console:
 console.log(20 / 4);
+5
 // log the result of the modulo of 10 into 54:
 console.log(10 % 54);
+10
