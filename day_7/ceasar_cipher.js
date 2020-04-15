f9e141eb88f9db0f1c7aef479be5b22068a83bde
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-04-15 10:23:42 -0600

    Update day 7 ceasar_cipher code

--- day_7/ceasar_cipher.js
+++ day_7/ceasar_cipher.js
@@ -25,38 +25,3 @@ function caesarCipher(str) {
 }
 
 caesarCipher("Hello, world!")
-
-
-/*class CaesarCipher {
-  constructor(string, leftShiftValue) {
-    this.string = string;
-    this.leftShiftValue = leftShiftValue;
-  }
-
-  encode() {
-
-      for (i = 0; i <= 12; i++) {
-
-        if (this.string.charCodeAt(i) >= 65 + leftShiftValue && str.charCodeAt(i) <= 90) {
-          string.push(String.fromCharCode(str.charCodeAt(i) - leftShiftValue));
-        }
-        else if (this.string.charCodeAt(i) >= 97 + leftShiftValue && str.charCodeAt(i) <= 122) {
-          string.push(String.fromCharCode(str.charCodeAt(i) - leftShiftValue));
-        }
-        else if (this.string.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 65 + leftShiftValue) {
-          string.push(String.fromCharCode(str.charCodeAt(i) + 26 - leftShiftValue));
-        }
-        else if (this.string.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 97 + leftShiftValue) {
-          string.push(String.fromCharCode(str.charCodeAt(i) + 26 - leftShiftValue));
-        }
-        else if (this.string.charCodeAt(i) < 65){
-          string.push(this.string[i]);
-        }
-      console.log(string.join(""))
-    }
-  }
-}
-
-var cipher = new CaesarCipher();
-cipher.encode("Hello World", 5);
-*/
