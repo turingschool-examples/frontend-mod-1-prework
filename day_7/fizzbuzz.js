de4c22c4222b2d88130a12661e180c8336dcef0a
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-14 20:37:42 -0600

    Add bonus solution to fizzbuzz.js

--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -15,3 +15,25 @@ for (i = 1; i <= 100; i++)
    console.log(i);
  }
 }
+
+// Bonus: Can you write the program so that it will run for any range of numbers?
+// I've written the program as a function that takes two arguments, lowerLimit and upperLimit so it can run for any range of numbers that are passed as arguments.
+
+function fizzBuzz(lowerLimit, upperLimit) {
+  for (i = lowerLimit; i <= upperLimit; i++)
+{
+ if ((i%3) === 0 && (i%5) === 0) {
+   console.log('Fizzbuzz');
+ }
+ else if (i % 3 === 0) {
+   console.log('Fizz');
+ }
+ else if (i % 5 === 0) {
+   console.log('Buzz');
+ }
+ else {
+   console.log(i);
+ }
+}
+}
+fizzBuzz(500, 520);
