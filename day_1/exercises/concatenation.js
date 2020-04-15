663b915e1f9add389c7a67268ab8e849188f9365
Stephanie Norton <steph.jane.norton@gmail.com>
2020-04-15 12:48:27 -0600

    Update answer

--- day_1/exercises/concatenation.js
+++ day_1/exercises/concatenation.js
@@ -16,6 +16,8 @@ var number = 7;
 var creatures = "unicorns";
 
 console.log("My zoo has " + number + " " + creatures + "!")
+// logs a string "My zoo has 7 unicorns!" as a result of type coercion.
+
 
  // Write code that combines the variables below into a string that
  // reads "The quick red fox jumped over the lazy brown dog":
@@ -31,4 +33,4 @@ console.log("The " + speedy + " jumped over the " + slowPoke);
 slowPoke = "tortoise";
 speedy = "hare";
 
-console.log("In a predictable result, the " + slowPoke + " beat the " + speedy);
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy!);
