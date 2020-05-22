51f5e63256223032abcd1a7cee043bae47af07a8
Chriscastanuela <chriscastanuela@ymail.com>
2020-05-22 17:57:22 -0600

    Add Day 2 Work

--- day_2/exercises/array.js
+++ day_2/exercises/array.js
@@ -10,36 +10,41 @@ var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals);
 
 // Write code below that will log an array of foods. Store the array in a variable.
-// YOUR CODE HERE
+var foods = ["banana", "peach", "pear", "plum"];
+console.log(foods);
 
-// example: Write code below that will log the number of elements in array of
-// animals from above.
+// example: Write code below that will log the number of elements
+//in array of animals from above.
 console.log(animals.length);
 
-// Write code below that will log the number of elements in the array of
-// foods from above.
-// YOUR CODE HERE
+// Write code below that will log the number of elements in the
+//array of foods from above.
+console.log(foods.length);
 
 // Write code below that will log "Zebra" from the animals array
-// YOUR CODE HERE
+console.log(animals.shift());
 
-// Write code using bracket notation that will log the first item in the animals array
-// YOUR CODE HERE
+// Write code using bracket notation that will log the first item
+//in the animals array
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the last item in the animals array
-// YOUR CODE HERE
+console.log(animals[1]);
 
 // Write code using bracket notation that will reassign the last item in the animals
 // array to "Gorilla"
-// YOUR CODE HERE
+animals[2] = "Gorilla"
+console.log(animals[2]);
 
 // Write code below that will log the last item from the foods array.
-// YOUR CODE HERE
+console.log(foods[3]);
 
 // Write code below that uses a method to add "lion" to the animals array and
 // log the result
-// YOUR CODE HERE
+animals.push("lion");
+console.log(animals);
 
 // Write code below that removes the last item of food from the foods array and
 // log the result
-// YOUR CODE HERE
+foods.pop();
+console.log(foods);
--- day_2/questions.md
+++ day_2/questions.md
@@ -1,17 +1,32 @@
 ## Day 2 Questions
 
 1. Create an array containing the following strings: `"zebra", "giraffe", "elephant"`.
-
-1. Save the array you created above to a variable `animals`.
-
-1. using the array `animals`, how would you access `"giraffe"`?
-
-1. How would you add `"lion"` to the `animals` array?
-
-1. Name and describe two additional array methods.
-
-1. What are the boolean values in JavaScript?
-
-1. In JavaScript, how would you evaluate if `2` is equal to `25`? What is the result of this evaluation?
-
-1. In JavaScript, how would you evaluate if `25` is greater than `2`? What is the result of this evaluation?
+```
+["zebra", "giraffe", "elephant"];
+```
+2. Save the array you created above to a variable `animals`.
+```
+var animals = ["zebra", "giraffe", "elephant"];
+```
+3. using the array `animals`, how would you access `"giraffe"`?
+```
+console.log(animals[1]);
+```
+4. How would you add `"lion"` to the `animals` array?
+```
+animals.push("lion");
+```
+5. Name and describe two additional array methods.
+- Pop
+- The "pop" method removes the last value from an array and logs the removed value to the console
+- shift
+- The "shift" method removes the first value from an array and logs the removed value to the console
+6. What are the boolean values in JavaScript?
+- A boolean value is a value of either "true" or "false"
+7. In JavaScript, how would you evaluate if `2` is equal to `25`? What is the result of this evaluation?
+```
+console.log(2 == 25);
+```
+8. In JavaScript, how would you evaluate if `25` is greater than `2`? What is the result of this evaluation?
+```
+console.log(25 > 2);
