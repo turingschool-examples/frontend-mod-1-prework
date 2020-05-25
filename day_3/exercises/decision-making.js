1ce3519514ae3a9a0a09e9a2c85617445a30c317
Matthew Lane <themplane@gmail.com>
2020-05-24 18:48:13 -0700

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -41,15 +41,36 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+Between lines 17 and 21, we hit our first conditional. At this point, the variable name doorChoice is set
+to the number 1, so therefore the if conditional would resolve to true. It executes the corresponding
+block of code, and sets the variable named bearClothing to the string `hat`. Since the it's an if/else,
+the rest of lines 19 through 21 don't execute and we continue from line 23.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
-4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+If doorChoice is anything that isn't the number 1, bearClothing will be changed to the string `scarf`
+
+4. In English, using technical vocabulary, describe what is happening between lines lines 30 and 38.
+
+Between lines 30 and 38, a series of if/elif/else conditionals are set up. If bearChoice is set to the number 1
+it prints a message saying that you offer your hat or scarf to the bear and he helps you. If bearChoice is set
+to the number 2, it prints a message that you insult the bear's hat or scarf and it cries. If bearChoice is set
+to the number 3, it prints a message saying you run to a different room with snakes in it. If bearChoice is
+anything else, it prints a message that says you befriend the bear.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+It logs to the console the string "You run as fast as you can into the next room. It's full of snakes!".
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+It prints the message "You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+
+I like hats, I like bears wearing hats, and I like friends, so doorChoice = 1, and bearChoice is not 1, 2, or 3.
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,39 @@
 
 1. What is a conditional statement? Give three examples.
 
+A conditional statement resolves to true or false depending on the state of the code when it runs.
+
+-( true == false ), resolves to false since they are not equal
+-( positiveInteger >= nonNegativeInteger && ( booleanVariable ) ), resolves to true if and only if booleanVariable is truthy and positiveInteger is greater than or equal to nonNegativeInteger
+-( numberOfFingers === 5 ) resolves to true if numberOfFingers is 5
+
 1. Why might you want to use an if-statement?
+
+You don't always want to run every piece of code available, so an if-statement allows you to run it only when appropriate.
 
 1. What is the JavaScript syntax for an if statement?
 
+if ( < conditional statement > ){
+  [block of code]
+}
+
 1. How do you add multiple conditions to an if statement?
+
+You could nest if-statements, or you can use 'and', 'or', or 'xor' operators to link conditional statements.
 
 1. What is the JavaScript syntax for an if/else if/else statement?
 
+if ( < conditional statement > ){
+  [code]
+}
+else if ( < conditional statement > ) {
+  [code]
+}
+....[some number of else-if-statements]
+else {
+  [code]
+}
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+How long to run a loop forß. 
