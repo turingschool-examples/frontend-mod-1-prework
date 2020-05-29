ea063d6ccecb1e1f215e0a0b0e4bc096a2ac8358
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-05-29 15:31:57 -0600

    Add day 1

--- day_1/exercises/booleans.js
+++ day_1/exercises/booleans.js
@@ -7,11 +7,21 @@ file from your terminal with the command `node day_3/exercises/comparisons.js`
 // example: log to the console the data type of `true`:
 console.log(typeof true);
 
+returns boolean
+
 // example: log to the console the data type of 1 is equal to 1:
 console.log(typeof (1 == 1));
+
+returns boolean
 
 // example: log to the console the result of 1 is equal to 2:
 console.log(1 == 2);
 
+returns boolean false
+
 // example: log to the console the result of 7 is  not equal to 2:
 console.log(1 == 2);
+
+return boolean false
+Should this code be written with the `!==` operator to represent inequality?
+When written as unequal, then the console would print a value of true.
--- day_1/exercises/concatenation.js
+++ day_1/exercises/concatenation.js
@@ -12,17 +12,21 @@ console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 
 // What happens if you include other data types when you use concatination? What data type is logged after we run this code?
 
+A string is logged from the previous example.
+
 var number = 7;
 var creatures = "unicorns";
 
 console.log("My zoo has " + number + " " + creatures + "!")
+
+Logs "My zoo has 7 unicorns!"
 
  // Write code that combines the variables below into a string that
  // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
-console.log(/*YOUR CODE HERE*/);
+console.log("The " + speedy + " jumped over the " + slowPoke);
 
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
@@ -31,4 +35,4 @@ console.log(/*YOUR CODE HERE*/);
 slowPoke = "tortoise";
 speedy = "hare";
 
- // YOUR CODE HERE
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!");
