cbbd53b14cd0cda7a8fa3a9c56e1112e574795d6
Kristi Miller <kristi.ann.miller@gmail.com>
2020-06-04 11:18:58 -0600

    day_4 changes added

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,38 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
+
 function printName() {
-  console.log("Severus Snape");
-};
+  console.log("Kristi Miller")
+}
 
 printName();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
-};
+function printName2(firstName, lastName) {
+  console.log(firstName + " " + lastName)
+}
 
-printName("Albus Dumbledore");
+printName2('Kristi', 'Miller')
+
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+
+function numSum(num1, num2) {
+  console.log(num1 + num2)
+}
+
+numSum(3, 5)
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+
+function michaelBolton(blank1, blank2) {
+  console.log("When a " + blank1 + " loves a " + blank2)
+}
+
+michaelBolton('man', 'woman')
