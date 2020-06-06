bfde5bc7d8c9a5b0a4b2ef938f097ce5b6131b83
Michael Walker <Michael.e.walker.87@gmail.com>
2020-06-06 12:37:37 -0600

    Add day 5 work

--- day_5/exercises/objects.js
+++ day_5/exercises/objects.js
@@ -15,18 +15,53 @@ console.log(foods);
 
 // Write code below that will print an object of animals and their number
 // at the zoo. (an inventory of animals)
-var zoo = // YOUR CODE HERE
-console.log(zoo)
+var zoo = {
+  lions: 4,
+  tigers: 6,
+  bears: 2,
+  wingedMonkeys: 20,
+};
+
+console.log(zoo);
 
 // Using the zoo that you created above, print all the keys in the object.
-// YOUR CODE HERE
+
+console.log(Object.keys(zoo));
+
+/*
+This prints the names in the list with Object.key()
+this will print [ 'lions', 'tigers', 'bears', 'wingedMonkeys' ]
+*/
 
 // Using the zoo that you created above, print all the values in the object.
-// YOUR CODE here
+
+console.log(Object.values(zoo));
+
+/*
+by switching the word after Objects. to values this has updated what would be printed
+in the last questions we printed only the keys with the .values we are only printing the values
+*/
 
 // Using the zoo taht you created above, print the value of the first item in
 // the object
-// YOUR CODE HERE
+
+console.log(zoo.lions); //This will take the key lions and print the value for lions which is 4
 
 // Add an animal to the zoo object and print the updated object.
-// YOUR CODE HERE
+zoo.terrierDog = 1;
+zoo ['lonesomeDuck'] = 1;
+
+console.log(zoo);
+
+/* I found 2 ways to add a property to the list one uses the dot notation and the other
+uses bracket. I added toto the terrierDog using the dot notation and the lonesomeDuck of oz
+using the bracket notation the updated log prints
+{
+  lions: 4,
+  tigers: 6,
+  bears: 2,
+  wingedMonkeys: 20,
+  terrierDog: 1,
+  lonesomeDuck: 1
+}
+*/
