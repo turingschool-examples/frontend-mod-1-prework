ea01f7e6191d4e285d1d0e4d4924b9fde4426762
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-06-06 21:04:26 -0600

    Add Day 2 Work

--- day_2/exercises/array.js
+++ day_2/exercises/array.js
@@ -7,12 +7,14 @@ file from your terminal with the command `nope day_2/exercises/arrays.js`
 // example: write code below that will log an array of animals.
 // Store the array in a variable.
 
+
 ```var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals);
 ```
 
 
 // Write code below that will log an array of foods. Store the array in a variable.
+
 
 ```var foods=["Ramen", "Salsa", "Potato"]
 ```
@@ -21,6 +23,7 @@ console.log(animals);
 // example: Write code below that will log the number of elements in array of
 // animals from above.
 
+
 ```console.log(animals.length);
 ```
 
@@ -28,11 +31,13 @@ console.log(animals);
 // Write code below that will log the number of elements in the array of
 // foods from above.
 
+
 ```console.log(foods.length);
 ```
 
 
 // Write code below that will log "Zebra" from the animals array
+
 
 ```console.log(animals[0])
 ```
@@ -42,11 +47,13 @@ console.log(animals);
 
 // Write code using bracket notation that will log the first item in the animals array
 
+
 ```animals[0]
 ```
 
 
 // Write code using bracket notation that will log the last item in the animals array
+
 
 ```console.log(animals[2])
 ```
@@ -55,11 +62,13 @@ console.log(animals);
 // Write code using bracket notation that will reassign the last item in the animals
 // array to "Gorilla"
 
+
 ```animals.splice(2,1,"Gorilla");
 ```
 
 
 // Write code below that will log the last item from the foods array.
+
 
 ```console.log(foods[2])
 ```
@@ -67,6 +76,7 @@ console.log(animals);
 
 // Write code below that uses a method to add "lion" to the animals array and
 // log the result
+
 
 ```animals.push("lion")
 ```
@@ -78,5 +88,7 @@ console.log(animals);
 ```
 // Write code below that removes the last item of food from the foods array and
 // log the result
+
+
 ```console.log(foods.pop());
 ```
