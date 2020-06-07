739df0e60b991081afc3a7bb06f2dec658bea6fd
Dennis Clark <drclark1985@gmail.com>
2020-06-07 13:55:55 -0600

    Add day_7 work

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,8 @@
+var numOfMonkeys = 10;
+var nameOfMonkeys = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
+
+for (var i = numOfMonkeys; i > 1; i--) {
+  console.log( nameOfMonkeys[i - 1] + " little monkeys jumping on the bed, \n One fell of and bumped his head, \n Mama called the doctor and the doctor said, \n 'No more monkeys jumping on the bed!'");
+}
+
+  console.log( nameOfMonkeys[i - 1] + " little monkey jumping on the bed, \n He fell off and bumped his head, \n Mama called the doctor and the doctor said, \n 'Get those monkeys right to bed'");
