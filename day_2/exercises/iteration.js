e6ce0adc3906d5bfdc86645832910f73e8befa42
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-06-06 21:17:55 -0600

    Add Day 2 Work

--- day_2/exercises/iteration.js
+++ day_2/exercises/iteration.js
@@ -9,51 +9,50 @@ file from your terminal with the command `node day_2/exercises/iteration.js`
 // each animal.
 
 
-```var animals = ["Zebra", "Giraffe", "Elephant"];
+var animals = ["Zebra", "Giraffe", "Elephant"];
 
-```for (var i = 0; i < animals.length; i++) {
+for (var i = 0; i < animals.length; i++) {
   console.log(animals[i]);
-}
-```
+  }
+
 
 
 // Write code below that iterates through a list of animals and prints "The
 // <animal> is awesome!" for each animal.
 
 
-```for (var i = 0; i < animals.length; i++) {
+for (var i = 0; i < animals.length; i++) {
    console.log("The "+animals[i]+" is awesome!");
-}
-```
+   }
+
 
 
 // Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
 
 
-```var foods=["Soup","Cheese","Butter"]
-```
+var foods=["Soup","Cheese","Butter"]
 
 
-```for (var i=0; i<foods.length; i++){
+for (var i=0; i<foods.length; i++){
    console.log("Add "+foods[i]+" to shopping list");
-... }
-```
+   }
+
 
 // Create an array of numbers and then iterate over that array to log doubles
 // of each of the number.
 
 
-```for (var i = 0; i < 5; i++){
+   for (var i = 0; i < 5; i++){
    console.log([i]*2);
    }
-   ```
 
 
-```var numbers=[3,6,9,12]
-```
 
-```for (var i=0;i<numbers.length; i++){
+var numbers=[3,6,9,12]
+
+
+for (var i=0;i<numbers.length; i++){
    console.log(numbers[i]*2);
    }
-   ```
+   
