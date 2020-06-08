65fc799e0fd018fb179fc719e6dcc62d0e549d65
Cameron Romo <63012953+cameronRomo@users.noreply.github.com>
2020-06-07 19:18:16 -0600

    Update comparisons.js

--- day_2/exercises/comparisons.js
+++ day_2/exercises/comparisons.js
@@ -13,13 +13,13 @@ var fourthVar = 0;
 console.log(firstVar > secondVar); // this should return: false
 
 // log the result of the comparison: is firstVar less than secondVar?
-``
+
 console.log(firstVar < secondVar); // this should return: true
-``
+
 // log the result of the comparison: is firstVar equal to thirdVar? (use the == operator)
-``
+
 console.log(firstVar == thirdVar); // this should return: true
-``
+
 /*
 note: this is an example of type coercion. Although thirdVar is a string and secondVar is an integer,
 this statement still evaluated to true. Javascript forces the thirdVar into an integer to
@@ -27,9 +27,9 @@ perform this evaluation
 */
 
 // log the result of the comparison: is firstVar strictly equal to thirdVar? (use the === operator)
-``
+
 console.log(firstVar === thirdVar); // this should return: false
-``
+
 /*
 note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the firstVar is an integer value and the thirdVar is a string value, although they are both equal to 10,
@@ -37,36 +37,36 @@ the comparison still evaluates to false.
 */
 
 // log the result of the comparison: is firstVar not equal to secondVar?
-``
+
 console.log(firstVar != secondVar); // this should return: true
-``
+
 
 // log the result of the comparison: is secondVar greater than or equal to 20?
-``
+
 console.log(secondVar >= 20); // this should return: true
-``
+
 // log the result of the comparison: is secondVar greater than or equal to 21?
-``
+
 console.log(secondVar >= 21); // this should return: false
-``
+
 // log the result of the comparison: is secondVar less than or equal to 20?
-``
+
 console.log(secondVar <= 20); // this should return: true
-``
+
 // log the result of the comparison: is secondVar less than or equal to 21?
-``
+
 console.log(secondVar <= 21); // this should return: true
-``
+
 //TODO: Is this a bit wonky of an example?
 
 // log the result of the comparison: is 0 equal to true?
-``
+
 console.log(0 == true) // this should return: false
-``
+
 // note: this is an example of a falsy value. In an evalution like this, 0 is considered to be false
 
 // log the result of the comparison: is 1 equal to true?
-``
+
 console.log(1 == true) // this should return: true
-``
+
 // note: this is an example of a truthy value. In an evalution like this, 1 is considered to be true
