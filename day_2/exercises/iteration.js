ca1817b0faaf0da66358d97d8296f1796e8cb479
Cameron Romo <63012953+cameronRomo@users.noreply.github.com>
2020-06-07 19:24:00 -0600

    Update iteration.js

--- day_2/exercises/iteration.js
+++ day_2/exercises/iteration.js
@@ -6,35 +6,35 @@ file from your terminal with the command `node day_2/exercises/iteration.js`
 
 // example: Write code below that iterates through a list of animals and print
 // each animal.
-``
+
 var animals = ["Zebra", "Giraffe", "Elephant"];
 
 for (var i = 0; i < animals.length; i++) {
   console.log(animals[i]);
 }
-``
+
 // Write code below that iterates through a list of animals and prints "The
 // <animal> is awesome!" for each animal.
-``
+
 for (var i = 0; i < animals.length; i++) {
   console.log(animals[i] + " is awesome!");
 }
-``
+
 // Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
-``
+
 var foods = ["Apples", "Almonds", "Carrots", "Chicken"];
 
 for (var i = 0; i < foods.length; i++) {
   console.log("Add " + foods[i] + " to shopping list");
 }
-``
+
 // Create an array of numbers and then iterate over that array to log doubles
 // of each of the number.
-``
+
 var numArray = [2, 4, 6, 8];
 
 for (var i = 0; i < numArray.length; i++) {
   console.log(numArray[i] * 2);
 }
-``
+
