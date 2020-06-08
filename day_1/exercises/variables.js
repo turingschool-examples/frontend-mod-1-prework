8b25ebcd6601098781a03e43d1f7caef4436a95f
Cameron Romo <63012953+cameronRomo@users.noreply.github.com>
2020-06-07 18:46:51 -0600

    Update variables.js

--- day_1/exercises/variables.js
+++ day_1/exercises/variables.js
@@ -3,32 +3,32 @@
 
 // example: Write code below to assign your name to a variable and
 // log that variable to the console:
-```JavaScript
+
 var name = "Harry Potter";
 console.log(name);
-```
+
 // Write code below to assign the string 'Dobby' to a variable and
 // log that variable to the console:
-```JavaScript
+
 var houseElf = "Dobby";
 console.log(houseElf)
-```
+
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console:
-```JavaScript
+
 var exspell = 'Harry Potter must not return to Hogwarts!';
 console.log(exspell);
-```
+
 // example: Write code below to add 2 to the variable `students` and
 // log the result:
-```JavaScript
+
 var students = 22;
 students += 2;
 console.log(students);
-```
+
 // Write the code below to subtract 2 students from the `students` variable and
 // log the result:
-```JavaScript
+
 students -= 2;
 console.log(students);
-```
+
