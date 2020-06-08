e953a3ecc1f316f8a69b0d8939df38db908886e5
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-06-07 22:59:44 -0600

    Add space between paragraphs in loop

--- day_7/10_little_monkeys.js
+++ day_7/10_little_monkeys.js
@@ -1,5 +1,6 @@
 var monkeyNumber = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
     for (var i = 0; i < monkeyNumber.length; i++){
         console.log(monkeyNumber[i] + " little monkeys jumping on the bed, \nOne fell off and bumped his head, \nMama called the doctor and the doctor said, \n'No more monkeys jumping on the bed!'");
+        console.log(" ");
       }
       console.log("One little monkey jumping on the bed, \nHe fell off and bumped his head, \nMama called the doctor and the doctor said, \n'Get those monkeys right to bed!'");
