acbe586a26a486b7a9206399386d8ff1d271bc5e
Stacy Potten <stacylynnaz@gmail.com>
2020-06-09 14:03:10 -0600

    Add Day 6 Work

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,4 +8,31 @@ person methods below the class so that they print their result to the
 terminal.
 */
 
-// YOUR CODE HERE
+class Daughter {
+  constructor(name, age, awake, favoriteSnack) {
+    this.name = name;
+    this.age = age;
+    this.awake = true;
+    this.favoriteSnack = favoriteSnack;
+  }
+
+  giveSnack(favoriteSnack) {
+    this.favoriteSnack = favoriteSnack;
+  }
+
+  putToBed() {
+    this.awake = false;
+  }
+
+};
+
+var firstChild = new Daughter("Olivia", 9, true, "Zbar");
+
+firstChild.giveSnack("Zbar");
+firstChild.putToBed();
+
+
+console.log(firstChild.name);
+console.log(firstChild.age);
+console.log(firstChild.awake);
+console.log(firstChild.favoriteSnack);
