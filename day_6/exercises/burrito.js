03ae0bb70f78a7d0c7b746a232bf57d6290c6017
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-06-09 23:39:14 -0400

    Add prompts loops functions

--- day_6/exercises/burrito.js
+++ day_6/exercises/burrito.js
@@ -1,15 +1,3 @@
-/*
-In the exercises below, write your own code where indicated
-to achieve the desired result. You should be able to run this
-file from your terminal with the command `node day_4/exercises/burrito.js`
-
-Add the following methods to this burrito class and call the methods below
-the class:
-1. addTopping
-2. removeTopping
-3. changeProtein
-*/
-
 class Burrito {
   constructor(protein, base, toppings) {
     this.protein = protein;
@@ -20,19 +8,44 @@ class Burrito {
     this.toppings.push(toppings);
   }
   removeTopping(toppings){
-    this.toppings.pop(toppings);
+    this.toppings = newTop;
   }
   changeProtein(protein){
-    this.protein = `chicken`;
+    this.protein = protein;
   }
 };
 
-var dinner = new Burrito("Beans", "Rice", ["cheese", "salsa", "guacamole"]);
+var base = prompt('Choose your base');        // prompts for base
+var protein = prompt('Choose your protein');  //prompts for protein
+var toppings = [];
+for(var i = 0; i <3; i++)
+  toppings.push(prompt('Choose your toppings'));    // prompts for 3 toppings, sets to array
 
-dinner.addTopping('queso');
-dinner.removeTopping();
-dinner.changeProtein();
+var dinner = new Burrito(protein, base, toppings);              // create new instance of Burrito
+console.log(dinner.base, dinner.protein, dinner.toppings);
 
-console.log(dinner.protein);
-console.log(dinner.base);
+var extraTop = prompt('you get a free topping, what shall it be!?'); //prompts to add topping
+dinner.addTopping(extraTop);                                        // pushes in new topping at end of array
 console.log(dinner.toppings);
+
+var removeTop = prompt('Do you want to remove any toppings now?'); //prompts to remove topping
+var newTop = toppings.filter(i => i !== removeTop);               //filters out selection array based on user selection (this is wicked sweet)
+dinner.removeTopping(newTop);
+
+if (confirm('Would you like double meat?')) {         // asks user to select y/n for double meat
+  var doubleMeat = 'DOUBLE! '+ protein                // if yes, will prompt you got it and return the new protein value
+  console.log('You got it!');
+} else {                                             //// if no, will prompt maybe next time and not alter protein value
+  var doubleMeat = protein
+  console.log('Maybe next time!');
+}
+dinner.changeProtein(doubleMeat);                    // passes the arugment through the changeProtein method
+
+console.log(dinner.base + ",", dinner.protein + ",", dinner.toppings)       // logs dinner
+
+if (confirm('Does everything look good here?')){                          // prompt to complete order
+  var completeOrder = dinner
+  console.log('Proceed to checkout');
+} else{
+  console.log('Reload and lets start again')
+}
