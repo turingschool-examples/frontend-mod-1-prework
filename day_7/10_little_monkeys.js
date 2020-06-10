ab8008674d46e4f25167bdcc6d1b4c32475aa593
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-06-09 22:22:23 -0600

    Add Day 7 Work

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,22 @@
+var monkeys = ["ten", "nine", "eight", "seven", "six", "five", "four", "three", "two"];
+
+for (var i = 0; i < monkeys.length; i++){
+  console.log(monkeys[i] + " little monkeys jumping on the bed, \n One fell off and bumped his head, \n Mama called the doctor and the doctor said, \n 'No more monkeys jumping on the bed!'");
+}
+  console.log("One little monkey jumping on the bed, \n He fell off and bumped his head, \n Mama called the doctor and the doctor said, \n 'Get those monkeys right to bed!'");
+
+
+/*
+BONUS CODE:
+function rhyme(monkeys){
+  if (monkeys > 0){
+    for (var i = monkeys; i > 1; i--){
+      console.log((i) + " little monkeys jumping on the bed, \n One fell off and bumped his head, \n Mama called the doctor and the doctor said, \n 'No more monkeys jumping on the bed!'");
+    }
+    console.log("1 little monkey jumping on the bed, \n He fell off and bumped his head, \n Mama called the doctor and the doctor said, \n 'Get those monkeys right to bed!'");
+  }
+else (console.log("enter a number greater than zero"))
+};
+
+rhyme(20) //user can enter any number
+*/
