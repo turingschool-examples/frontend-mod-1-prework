6d10b01dd2b3342d45cc9a2c47066823e1ec0383
Stacy Potten <stacylynnaz@gmail.com>
2020-06-09 19:41:41 -0600

    Add monkey work

---
+++ day_7/10_little_monkeys.js
@@ -0,0 +1,13 @@
+var monkey = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+
+var poem = " little monkeys jumping on the bed, \nOne fell off and bumped his head, \nMama called the doctor and the doctor said, \nNo more monkeys jumping on the bed! \n"
+
+var finalMonkey = "One"
+
+var finalStanza = " little monkey jumping on the bed, \nHe fell off and bumped his head, \nMama called the doctor and the doctor said, \nGet those monkeys right to bed!"
+
+for (var i = 0; i < monkey.length; i++) {
+  console.log(monkey[i] + poem);
+}
+
+console.log(finalMonkey + finalStanza);
