9c49b6221904138a136f2b5c4b694d68dc5de093
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-06-16 17:35:10 -0600

    Add second attempt with less conditional logic

--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -4,6 +4,7 @@
 //multiple of both = "FizzBuzz"
 //others = console.log(number)
 
+//Original Attempt:
 for (testNumber = 1; testNumber <= 100; testNumber++) {
 
   if ((testNumber % 3 === 0) && (testNumber % 5 === 0)) {
@@ -23,109 +24,17 @@ for (testNumber = 1; testNumber <= 100; testNumber++) {
   }
 }
 
-//expected output:
+//Attempt after Feedback:
+//Goal is to use only one && statement and not to use "not equal to" in determining code
+for (testNumber = 1; testNumber <= 100; testNumber++) {
 
-/*
-1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-11
-Fizz
-13
-14
-FizzBuzz
-16
-17
-Fizz
-19
-Buzz
-Fizz
-22
-23
-Fizz
-Buzz
-26
-Fizz
-28
-29
-FizzBuzz
-31
-32
-Fizz
-34
-Buzz
-Fizz
-37
-38
-Fizz
-Buzz
-41
-Fizz
-43
-44
-FizzBuzz
-46
-47
-Fizz
-49
-Buzz
-Fizz
-52
-53
-Fizz
-Buzz
-56
-Fizz
-58
-59
-FizzBuzz
-61
-62
-Fizz
-64
-Buzz
-Fizz
-67
-68
-Fizz
-Buzz
-71
-Fizz
-73
-74
-FizzBuzz
-76
-77
-Fizz
-79
-Buzz
-Fizz
-82
-83
-Fizz
-Buzz
-86
-Fizz
-88
-89
-FizzBuzz
-91
-92
-Fizz
-94
-Buzz
-Fizz
-97
-98
-Fizz
-Buzz
-*/
-
-//Bonus:
+  if ((testNumber % 3 === 0) && (testNumber % 5 === 0)) {
+    console.log("FizzBuzz");
+    } else if (testNumber % 3 === 0) {
+      console.log("Fizz");
+      } else if (testNumber % 5 === 0) {
+        console.log("Buzz");
+        } else {
+          console.log(testNumber);
+        }
+      }
