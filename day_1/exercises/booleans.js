350a99a23dffa72e64d34cb96a875b171a900113
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-07-13 18:56:44 -0600

    Correct boolean

--- day_1/exercises/booleans.js
+++ day_1/exercises/booleans.js
@@ -11,7 +11,7 @@ console.log(true);
 console.log(1 == 1);
 
 // example: log to the console the result of 1 is equal to 2:
-console.log(1 == 2);
+console.log(1 != 2);
 
 // example: log to the console the result of 7 is  not equal to 2:
 console.log(7 != 2);
