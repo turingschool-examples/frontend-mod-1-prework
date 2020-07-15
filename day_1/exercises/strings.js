870e38e0e0e742c209a680658b495280a6af7243
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-07-15 15:16:01 -0600

    Update command and add one challenge

--- day_1/exercises/strings.js
+++ day_1/exercises/strings.js
@@ -1,7 +1,7 @@
 /*
 In the exercises below, write your own code where indicated
 to achieve the desired result. You should be able to run this
-file from your terminal with the command `node day_3/exercises/comparisons.js`
+file from your terminal with the command `node day_1/exercises/strings.js`
 */
 
 // example: Write code below to log your name in the console:
@@ -11,4 +11,7 @@ console.log("Alan Turing");
 console.log(/*YOUR CODE HERE*/);
 
 // Write code below to log `99 bottles of pop on the wall...`:
+// YOUR CODE HERE
+
+//Write code below to log one line from your favorite song or movie:
 // YOUR CODE HERE
