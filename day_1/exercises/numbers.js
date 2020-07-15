2070fa70e40b0968b27e0ae9d6d66f5f35b73d85
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-07-15 15:14:58 -0600

    Update command and add one challenge

--- day_1/exercises/numbers.js
+++ day_1/exercises/numbers.js
@@ -1,7 +1,7 @@
 /*
 In the exercises below, write your own code where indicated
 to achieve the desired result. You should be able to run this
-file from your terminal with the command `node day_3/exercises/comparisons.js`
+file from your terminal with the command `node day_1/exercises/numbers.js`
 */
 
 // example: log the result of the sum of 2 and 2 to the console:
@@ -17,4 +17,9 @@ console.log(/*YOUR CODE HERE*/);
 // YOUR CODE HERE
 
 // log the result of the modulo of 10 into 54:
+// YOUR CODE HERE
+
+// log the total number of snacks to the console, using the variables below:
+var healthySnackCount = 7;
+var junkFoodSnackCount = 4;
 // YOUR CODE HERE
