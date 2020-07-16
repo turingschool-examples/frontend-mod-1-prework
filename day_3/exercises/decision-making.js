5558c24a3b056d14788a0105dd742e8345bd8cfb
Olga Morgan <olga.morgan17@gmail.com>
2020-07-15 19:14:04 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -41,15 +41,47 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+The first condition of if statement is choosing the door that stricly equals number 1. If this condition is true then it executes
+the code between the first curly brackets which assigns the value "hat" to a variable bearClothing.
+Otherwise, if the first condition is false which includes any answer other than number 1, it executes the code between
+the second set of curly brackets which assigns the value "scarf" to the variable bearClothing.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+"scarf"
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+
+
+If the first condition is true where our choice strictly equals the first choice, the code executes the first
+set of curly brackets to log "You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!".
+
+If the first condition is false, the code is checking the second condition if our choice strictly equals the second choice.
+If this condition is true, it executes the code between the second set of curly brackets to log
+"You tell the bear the " + bearClothing + " is too small and it starts to cry!".
+
+If the first condition is false, the second condition is false, then the code starts checking the third condition
+if the choice we pick strictly equals the third choice. If condition number 3 is true, then it executes the code between the third set
+of curly brackets to log "You run as fast as you can into the next room. It's full of snakes!".
+
+Otherwise, If none of the three above conditions are true, it executes the code between the last set of curly
+brackets to log "You stay with the bear and become it's best friend!".
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+"You run as fast as you can into the next room. It's full of snakes!"
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+"You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+The first one where we get "You offer the bear your hat and the bear shows you a secret passage out!".
+It's a positive ending with an extra bonus to see the secret passage out! It doesn't say that we cannot become friends if we make the first choice either, so
+the possibilty to still become friends with a bear is not eliminated.
+
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,79 @@
 
 1. What is a conditional statement? Give three examples.
 
+Conditional statement checks a condition. If first condition is true, the code executes the first
+statement, if the condition is false, the code executes the second statement.  
+
+Example 1:
+```JavaScript
+var weather = "rainy";
+
+if (weather == "sunny") {
+  console.log ("Take glasses");
+}
+else {
+  console.log ("Leave glasses at home");
+}
+
+```
+Example 2:
+```JavaScript
+var chickenTemperature = 100;
+
+if ( chickenTemperature <= 140) {
+  console.log("Chicken is not ready!");
+}
+else if (chickenTemperature > 140 && chickenTemperature <165) {
+  console.log("Wait a little more!");
+}
+else {
+  console.log("It's ready to eat!")
+}
+```
+Example 3:
+```JavaScript
+var ageToVote = 22;
+
+if (ageToVote < 18) {
+  console.log("Not eligible to vote");
+}
+else {
+  console.log("Eligible to vote");
+}
+```
 1. Why might you want to use an if-statement?
+
+If I need to take an action based on a condition.
 
 1. What is the JavaScript syntax for an if statement?
 
+Use keyword if to specify the block of code if the condition is true. Use round brackets
+to specify condition after the keyword if. Then, use curly brackets to show the action that needs to be done if the condition is true.
+
+```Javascript
+if (condition1) {
+  //action1 if condition1 is true;
+}
+```
 1. How do you add multiple conditions to an if statement?
+
+We can add multiple conditions using logical operators || (OR) and && (AND).
 
 1. What is the JavaScript syntax for an if/else if/else statement?
 
+```Javascript
+if (condition1) {
+  //action1 if condition1 is true;
+}
+else if (condition2) {
+  //action2 if condition1 is false;
+}
+else {
+  //action3 if none of the listed conditions are true;
+}
+```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+We can use if/else and if/else if statements to take different actions based on different conditions. We can also try
+using conditional statement to check one variable at a time. 
