5ac06f7504adfce6eaf47eb3641aedc98143ce26
Ben Prat <65628677+benjaminprat@users.noreply.github.com>
2020-07-16 08:37:09 -0600

    Update variables.js

--- day_1/exercises/variables.js
+++ day_1/exercises/variables.js
@@ -3,26 +3,31 @@
 
 // example: Write code below to assign your name to a variable and
 // log that variable to the console:
+
 var name = "Ben Prat";
 console.log(name);
 
 // Write code below to assign the string 'Dobby' to a variable and
 // log that variable to the console:
+
 var houseElf = "Dobby";
 console.log(houseElf);
 
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console:
+
 var string = `Harry Potter must not return to Hogwarts!`;
 console.log(string)
 
 // example: Write code below to add 2 to the variable `students` and
 // log the result:
+
 var students = 22;
 var students = (22 + 2);
 console.log(students);
 
 // Write the code below to subtract 2 students from the `students` variable and
 // log the result:
+
 var students = (students - 2);
 console.log(students);
