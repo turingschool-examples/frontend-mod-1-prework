3c9dd042d5df5d739bec36f3358472ecdd49ab1b
Kara Caputo <kncaputo@gmail.com>
2020-07-18 15:40:58 -0400

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -10,7 +10,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 2;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -41,15 +41,43 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+   The *if* statment checks the conditional which checks if the value of the variable
+   named doorChoice is equal to one. If this condition is true, then code on line 18 runs and
+   the variable bearClothing is assigned to the string value of "hat". If the condition
+   is false, the code moves to the following statement. *Else* means for every conditional result
+   other than true, run line 20. Line 20 assigns the value for bearClothing to the string "scarf".
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+   bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+   scarf
+
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
 
-5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+   *If* it's true that the variable bearChoice equals 1, then line 31 will run. This tells the console
+   to log the message of the given string, plus the value of the variable bearClothing, plus the final given string,
+   and this would be the end point for the code in this particular conditional block.
+   *If* the conditional on line 30 was false, and the value of the variable bearChoice did not equal 1,
+   then the next conditional statement will be checked, line 32. If the value of the variable bearChoice is equal
+   to 2, line 33 will run and the console will log the message. If the conditional on line 32 is false, the
+   the conditional on line 34 will be checked. The value of the variable bearClothing will be checked to equal 3.
+   If true, the console will log the given string. If false, the console will log the message on line 37. Line 36
+   is saying, if you got this far, then log the following message to the console. This statement is not comparing
+   a conditional.
+
+5. If you changed the variable bearChoice to equal 3, what will be the final outcome be
+
+   The console will log the message, "You stay with the bear and become it's best friend."
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+   The console will log the message, "You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+
+   You stay with the bear and become it's best friend.
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,82 @@
 
 1. What is a conditional statement? Give three examples.
 
+   A conditional statement checks to see if a given condition is true before running a specific code block. If the original conditional is false, further statements can then be checked, each with a dependent result. An *else* statement can also be included at the end of the code block to execute a specific line of code in the event that none of the previous conditions were met.
+
+   For example:
+
+1. //If the variable value betterPet is dog, then the console will log "You're right". For everything else, the console will log, "You're wrong".
+
+   ```
+   var betterPet = "dog";
+
+   if (betterPet === "dog") {
+     console.log("You're right");
+   } else {
+     console.log("You're wrong");
+   }
+   ```
+
+2. //If the variable value chosenNum is greater than 10, then the console will log "You're right". For everything else, the console will log, "Not today".
+
+   ```
+   var chosenNum = 9;
+
+   if (chosenNum > 10) {
+     console.log("You're right");
+   } else {
+     console.log("Not today");
+   }
+   ```
+
+2. //If favColor is blue, then "the sky is blue". If favColor is brown, then "That's strange". For everything else, "I don't want to talk about colors"
+      ```JavaScript
+      var favColor = "";
+
+      if (favColor === blue) {
+        console.log("the sky is blue");
+      } else if (favColor === brown) {
+        console.log("That's strange");
+      } else {
+        console.log("I don't want to talk about colors");
+      }
+      ```
+
 1. Why might you want to use an if-statement?
 
+   You might want to use an if-statement if you want a specific block of code to run only if a certain condition is true.
+
 1. What is the JavaScript syntax for an if statement?
+   ```JavaScript
+   if () {
+
+   }
+   ```
 
 1. How do you add multiple conditions to an if statement?
 
+   To check if additional conditions are true, else/if statements can be used. Then to run a specific block of code if all the proceeding conditions are false, you can use an else statement.
+
 1. What is the JavaScript syntax for an if/else if/else statement?
 
+   ```JavaScript
+   if () {
+
+   } else {
+
+   }
+   ```
+
+   ```JavaScript
+   if () {
+
+   } else if () {
+
+   } else {
+
+   }
+   ```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+   A while-loop runs only if a specified condition is true. For example, *while* this is true, this happens.
