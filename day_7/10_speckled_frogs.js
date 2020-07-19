bc82097b8a918d4ff0813e647354751e962bce7a
Peyton Jones <peytonj.dev@gmail.com>
2020-07-18 23:52:58 -0500

    speckled frogs completed

---
+++ day_7/10_speckled_frogs.js
@@ -0,0 +1,13 @@
+function speckledFrogs () {
+  for (var i = 10; i > 0; i--) {
+    console.log( i + " speckled frogs sat on a log eating some most delicious bugs.")
+    console.log("One jumped in the pool where its nice and cool, " )
+    if (i === 1 ) {
+      console.log("then there were no more speckled frogs!")
+    } else {
+      console.log("then there were " + (i-1) + " speckled frogs.")
+    }
+  }
+}
+
+speckledFrogs()
