36333ca67a4609047877e9058e780c1fb082cc64
Max Bregman <bregmanmax91@gmail.com>
2020-07-19 15:36:47 -0600

    Add Day 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -3,28 +3,37 @@ In the exercises below, write your own code where indicated
 to achieve the desired result. You should be able to run this
 file from your terminal with the command `node day_4/exercises/functions.js`
 */
-
+/*
 //example: Write a function below that, when called will print your name
 function printName() {
-  console.log("Severus Snape");
+  console.log("Max B");
 };
 
 printName();
-
+*/
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
 function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+  console.log("Max B")
 };
 
 printName("Albus Dumbledore");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
-
+function addNumbers() {
+  console.log(1+2)
+}
+addNumbers()
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+function storyTime(){
+  console.log("When " + nameOne + " Met " + nameTwo)
+}
+var nameOne = "Bob"
+
+var nameTwo = "Lisa"
+
+storyTime()
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,15 +1,38 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a function?
+  * A function allows us to make a large block of code which does a specific task in multiple steps and then be able to call up that entire block with one name instead of having to write down all those blocks each time we need to use the function.
 
-1. What is a parameter? 
+1. What is a parameter?
+  * Pieces of information that are passed to a function.
 
 1. What is a return value?
+  * This is the response or information we get back from the function.
 
 1. In the space below, create a function named `hello` that will print `"Sam I am"`.
 
+```
+function hello() {
+  console.log("Sam I am")
+}
+
+hello();
+```
+
 1. Create a function name `hello_someone` that takes an argument of `name` and logs `name + " I am"`.
 
-1. How would you call or execute the function that you created above?
+```
+function hello_someone() {
+  console.log(name + " I am")
+}
 
+var name = "Sam"
+
+```
+
+1. How would you call or execute the function that you created above?
+```
+hello_someone()
+```
 1. What questions do you still have about functions in Javascript?
+  * None that I can articulate at the moment.
