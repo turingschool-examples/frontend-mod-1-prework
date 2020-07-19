53920567aec65fd3530929dd63c587ae7cb7561b
Peyton Jones <peytonj.dev@gmail.com>
2020-07-18 20:42:52 -0500

    solved fizzbuzz

---
+++ day_7/fizzbuzz.js
@@ -0,0 +1,16 @@
+
+function fizzBuzz() {
+  for(var i = 1; i < 101; i++ ) {
+    if (!(i%3) && !(i%5)) {
+      console.log("FizzBuzz")
+    } else if (!(i%3)) {
+      console.log("Fizz")
+    } else if (!(i%5)) {
+      console.log("Buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+fizzBuzz()
