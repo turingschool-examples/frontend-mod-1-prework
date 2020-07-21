fc2284ba66ead9afba4fd08bc01974f7f89a3354
Olga Morgan <olga.morgan17@gmail.com>
2020-07-20 18:06:32 -0600

    Fix Day 7 Work 10 Speckled Frogs

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -1,69 +1,64 @@
 
 var frogCount = 10;
   for (var i = frogCount; i >= 1; i--){
-    if (frogCount > 1) {
-      var message = `${frogCount} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were ${--frogCount} speckled frogs.`;
-
-       console.log(message);
+    var frogs = i;
+    var message = `${frogs} speckled frogs sat on a log \
+                   eating some most delicious bugs. \
+                   One jumped in the pool where its nice and cool,`;
+      console.log(message);
+    var message2 = "";
+    if (frogs > 1) {
+      message2 = `then there were ${frogs-1} speckled frogs.`;
     }
     else {
-      var message = `${frogCount} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were no more speckled frogs!`;
-
-       console.log(message);
+      message2 = `then there were no more speckled frogs!`;
     }
+     console.log(message2);
   }
+
+
 
 
 //Extension 1
 
+
 var numberToWord =  ["one", "two", "three","four","five","six","seven","eight","nine","ten"];
 
-  for (var i = numberToWord.length - 1; i >= 0; i--){
-    for (var j = numberToWord.length - 1; i >= 0; i--){
-    if(j >= 1) {
+  for (var i = numberToWord.length; i >= 1; i--){
+    var index = i - 1;
+    var message = `${numberToWord[index]} speckled frogs sat on a log \
+                 eating some most delicious bugs. \
+                 One jumped in the pool where its nice and cool,`;
+    console.log(message);
 
-     var message = `${numberToWord[j]} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were ${numberToWord[--j]} speckled frogs.`;
-       console.log(message);
+    var message2 = "";
+    if(index >= 1) {
+      message2 = `then there were ${numberToWord[index-1]} speckled frogs.`;
     }
     else {
-      var message = `${numberToWord[0]} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were no more speckled frogs!`;
-
-       console.log(message);
+      message2 = `then there were no more speckled frogs!`;
     }
-  }
+    console.log(message2);
+
 }
+
 
 //Extension 2
 
 function speckledFrogs (numOfFrogs) {
   for (var i = numOfFrogs; i >= 1; i--){
-    if (numOfFrogs > 1) {
-      var message = `${numOfFrogs} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were ${--numOfFrogs} speckled frogs.`;
-
-       console.log(message);
+    var frogs = i;
+    var message = `${frogs} speckled frogs sat on a log \
+                   eating some most delicious bugs. \
+                   One jumped in the pool where its nice and cool,`;
+      console.log(message);
+    var message2 = "";
+    if (frogs > 1) {
+      message2 = `then there were ${frogs-1} speckled frogs.`;
     }
     else {
-      var message = `${numOfFrogs} speckled frogs sat on a log \
-                    eating some most delicious bugs. \
-                    One jumped in the pool where its nice and cool,\
-                    then there were no more speckled frogs!`;
-
-       console.log(message);
+      message2 = `then there were no more speckled frogs!`;
     }
+     console.log(message2);
   }
 }
