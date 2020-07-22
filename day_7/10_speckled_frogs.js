d35374dbc8226b270de996de234fb2666367e6e2
Chung Hong <conconart.info@gmail.com>
2020-07-21 20:51:48 -0600

    Add Day 7 iteration projects

---
+++ day_7/10_speckled_frogs.js
@@ -0,0 +1,9 @@
+for (var i=10; i>0; i--) {
+  if (i > 2) {
+  console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where it was nice and cool, \n then there were " + (i - 1) + " speckled frogs.");
+} else if (i === 2) {
+  console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where it was nice and cool, \n there there was one speckled frog.");
+} else {
+  console.log(i + " speckled frog sat on a log \n eating some most delicious bugs. \n One jumped in the pool where it was nice and cool, \n then there were no more speckled frogs!")
+}
+};
---
+++ day_7/fizzbuzz.js
@@ -0,0 +1,13 @@
+
+for(var i=1; i<100; i++) {
+  var skip = 0;
+  if ((i % 3 == 0) && (i % 5 == 0)) {//multiple of 3 and 5
+    console.log("FizzBuzz");
+  } else if(i % 3 == 0) { //multiple of 3
+    console.log("Fizz");
+  } else if (i % 5 == 0) {//multiple of 5
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+};
