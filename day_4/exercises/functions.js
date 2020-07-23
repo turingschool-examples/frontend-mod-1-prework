12a87fd5a39b11b105d4e1d1fa6ccd7b327b8759
Peyton Jones <peytonj.dev@gmail.com>
2020-07-22 20:28:21 -0500

    first round changes made

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,41 +5,41 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
+function printName(name) {
   console.log("Peyton Jones");
 };
 
-printName();
+printName("Peyton Jones");
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() {
-  console.log("Peyton Jones")
+function printName(name) {
+  console.log(name)
 };
 
 printName("Peyton Jones");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-function numSum() {
-  console.log(5 * 10)
+function numSum(num1+num2) {
+  console.log(num1+num2)
 };
-numSum()
+numSum(5, 10)
 
 // Write a function that takes in two strings and prints a concatenation
 
 function makeSentence(string, string1) {
-  console.log("I love to" + " swim")
+  console.log(string,string1)
 };
 
-makeSentence()
+makeSentence("I love to" + " swim")
 
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
 
 function firstIntro (man, woman) {
-  console.log("When Harry met Sally")
+  console.log(man, woman)
 };
 
-firstIntro()
+firstIntro("Harry", "Sally")
