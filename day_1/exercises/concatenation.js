450d8fe619dd46d773276b467ad13e5414eda7bf
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-07-23 10:19:04 -0600

    Add day 1

--- day_1/exercises/concatenation.js
+++ day_1/exercises/concatenation.js
@@ -11,24 +11,24 @@ var team = "Chudley Cannons";
 console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 
 // What happens if you include other data types when you use concatination? What data type is logged after we run this code?
-
 var number = 7;
 var creatures = "unicorns";
 
 console.log("My zoo has " + number + " " + creatures + "!")
+console.log("When string and number data types are concatenated, the resulting data type logged is a string")
 
  // Write code that combines the variables below into a string that
  // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
-console.log(/*YOUR CODE HERE*/);
+console.log("The " + speedy + " jumped over the " + slowPoke);
 
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
-slowPoke = "tortoise";
-speedy = "hare";
+var slowPoke2 = "tortoise";
+var speedy2 = "hare";
 
- // YOUR CODE HERE
+console.log ("In a predictable result, the " + slowPoke2 + " beat the" + speedy2);
--- day_1/exercises/variables.js
+++ day_1/exercises/variables.js
@@ -3,25 +3,27 @@
 
 // example: Write code below to assign your name to a variable and
 // log that variable to the console:
-var name = "Harry Potter";
+var name = "Lola Dolinsky";
 console.log(name);
 
 // Write code below to assign the string 'Dobby' to a variable and
 // log that variable to the console:
 var houseElf = "Dobby";
-// YOUR CODE HERE
+console.log(houseElf);
 
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console:
-// YOUR CODE HERE
+var message = "Harry Potter must not return to Hogwarts!";
+console.log (message);
 
 // example: Write code below to add 2 to the variable `students` and
 // log the result:
 var students = 22;
-// YOUR CODE HERE
-console.log(students);
+var students2 = 2;
+console.log(students + students2);
 
 // Write the code below to subtract 2 students from the `students` variable and
 // log the result:
-// YOUR CODE HERE
-console.log(students);
+var students = 22;
+var students2 = 2;
+console.log(students - students2);
