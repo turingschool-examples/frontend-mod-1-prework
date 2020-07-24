e94245c51da9025396c46f97d4d5656cc0e41c67
Peyton Jones <peytonj.dev@gmail.com>
2020-07-23 19:11:52 -0500

    person updates complete

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,21 +8,23 @@ person methods below the class so that they print their result to the
 terminal.
 */
 class Person {
-  constructor (hair, eyes) {
+  constructor (hair, height) {
     this.hair = hair;
     this.height = height;
   }
   dyeHair (newColor) {
     this.hair = newColor
-    console.log(this.hair)
+
   }
   wearHeels(heelSize) {
-    this.height = heelSize + this.height
-    console.log(this.height)
+    this.height = this.height + heelSize
+    
   }
 };
 
 var jessica = new Person("red","5 feet" )
+console.log(jessica)
 
-person.dyeHair("brown")
-person.wearHeels("6 inch")
+jessica.dyeHair("brown")
+jessica.wearHeels(" 6 inches")
+console.log(jessica)
