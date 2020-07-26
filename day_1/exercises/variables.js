7e186f61b391af7d378d6677a05a4a78f8f429e4
Whitney Wilkes <wilkes.whit@gmail.com>
2020-07-25 23:22:42 -0600

    Add day 1

--- day_1/exercises/variables.js
+++ day_1/exercises/variables.js
@@ -1,9 +1,9 @@
 // In the exercises below, write your own code where indicated
-// to achieve the desired result. Run this code using node in terminal.
+// to achieve the desired result. Run this file using node in terminal to check the output.
 
 // example: Write code below to assign your name to a variable and
 // log that variable to the console:
-var name = "Harry Potter";
+var name = "Whitney Wilkes";
 console.log(name);
 
 // Write code below to assign the string 'Dobby' to a variable and
@@ -13,17 +13,16 @@ console.log(houseElf)
 
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console:
-var alternative = "Harry Potter must not return to Hogwarts!"
-console.log(alternative)
+var wrong = "Harry Potter must not return to Hogwarts!"
+console.log(wrong)
 
 // example: Write code below to add 2 to the variable `students` and
 // log the result:
-var students = 22
+var students = 22;
 students += 2
-console.log(students)
+console.log(students);
 
 // Write the code below to subtract 2 students from the `students` variable and
 // log the result:
-var students = 22
 students -= 2
-console.log(students)
+console.log(students);
