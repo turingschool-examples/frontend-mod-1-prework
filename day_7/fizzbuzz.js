f0a1be7f1878cfc1f3f21f368550f3c9b7c98302
Bruce Gordon <brc.gordon@gmail.com>
2020-07-27 10:59:08 -0600

    Add Fizz for 3 multiples

---
+++ day_7/fizzbuzz.js
@@ -0,0 +1,12 @@
+function fizzBuzz() {
+  for (var i = 1; i < 101; i++) {
+   if (i % 3 == 0) {
+    console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz();
