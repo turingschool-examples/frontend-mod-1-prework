d21b94961293af8d596893d7a9b520988062f88b
Whitney <65980084+whitneywilkes@users.noreply.github.com>
2020-07-26 19:22:54 -0600

    Update if-statements.js

--- day_3/exercises/if-statements.js
+++ day_3/exercises/if-statements.js
@@ -10,29 +10,20 @@ with you to be prepared based on the following conditions:
  * if it is icy, log "yak traks"
  ____
  
-> var weather = 'snowy';
+ > var weather = 'snowy'
 
 > if (weather == 'sunny') {
-
 >  console.log("sunscreen");
-
 > } else if (weather == 'rainy') {
-  
 >  console.log("umbrella");
-  
 > } else if (weather == 'snowy') {
-  
 >  console.log("coat");
-  
 > } else if (weather == 'icy') {
-  
 >  console.log("yak traks");
-  
 > } else{
-  
 > console.log("good to go!");
-  
 > }
+
 
 Manipulate the variable 'weather' to see if you can print something other
 than 'coat'.
