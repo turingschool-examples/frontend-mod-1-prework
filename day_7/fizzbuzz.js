7be89f4eb27e22b78c36420cdd0f13049bf2d5f6
Bruce Gordon <brc.gordon@gmail.com>
2020-07-27 11:08:56 -0600

    Write code for multiples 3 and 5

--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -1,9 +1,11 @@
 function fizzBuzz() {
   for (var i = 1; i < 101; i++) {
-   if (i % 3 == 0) {
+   if (i % 3 == 0 && i % 5 !== 0) {
     console.log("Fizz");
-    } else if (i % 5 == 0) {
+  } else if (i % 5 == 0 && i % 3 !== 0) {
         console.log("Buzz");
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
     } else {
       console.log(i);
     }
