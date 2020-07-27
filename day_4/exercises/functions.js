6dd4c1b8cb0a384be50129c060d680ef92ed156e
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-07-27 11:35:27 -0600

    Update Day 4 exercise answers

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -6,7 +6,7 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 
 //example: Write a function below that, when called will print your name
 function printName() {
-  console.log("Kelsie");
+  console.log("Kelsie Besinger-Yeh");
 };
 
 printName(); //prints out my name
@@ -14,11 +14,11 @@ printName(); //prints out my name
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printFullName(first, last) { // hint.. add a parameter on this line :)
-  console.log("Kelsie", "Besinger-Yeh");
+function printFullName(fullName) { // hint.. add a parameter on this line :)
+  console.log(fullName);
 };
 
-printFullName("Kelsie", "Besinger-Yeh");
+printFullName("Kelsie Besinger-Yeh");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
@@ -31,8 +31,8 @@ area(2, 4);
 
 
 // Write a function that takes in two strings and prints a concatenation
-// of those two strings, for example the arguments could be (man, woman) and
-// the end result might be "When Harry Met Sally".  Then, call that function.
+// of those two strings, for example the arguments could be ("Oscar", "Ruck") and
+// the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
 function food(food1, food2) {
   console.log("My favorite foods are " + food1 + " and " + food2 +"");
