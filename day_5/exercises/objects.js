c501c031ffdf92bdd541050efb547d32fcbe7498
Whitney Wilkes <wilkes.whit@gmail.com>
2020-07-26 23:23:42 -0600

    Add day 5

--- day_5/exercises/objects.js
+++ day_5/exercises/objects.js
@@ -1,7 +1,7 @@
 /*
 In the exercises below, write your own code where indicated
 to achieve the desired result. You should be able to run this
-file from your terminal with the command `node day_4/exercises/objects.js`
+file from your terminal with the command `node day_5/exercises/objects.js`
 */
 
 // example: Write code below to print an object that holds grocery store inventory
@@ -13,20 +13,35 @@ var foods = {
 
 console.log(foods);
 
-// Write code below that will print an object of animals and their number
-// at the zoo. (an inventory of animals)
-var zoo = // YOUR CODE HERE
-console.log(zoo)
+// Write code below that will print an B=Object of animals and the number
+// of that type of animal at the zoo. (an inventory of animals)
 
-// Using the zoo that you created above, print all the keys in the object.
-// YOUR CODE HERE
+var zoo = {
+  lions: 4,
+  tigers: 5,
+  bears: 8,
+  otters: 12
+};
 
-// Using the zoo that you created above, print all the values in the object.
-// YOUR CODE here
+console.log(zoo);
 
-// Using the zoo taht you created above, print the value of the first item in
-// the object
-// YOUR CODE HERE
+// Using the zoo that you created above, print all the keys in the Object.
 
-// Add an animal to the zoo object and print the updated object.
-// YOUR CODE HERE
+var animals = Object.keys(zoo);
+
+console.log(animals);
+
+// Using the zoo that you created above, print all the values in the Object.
+
+var animals = Object.values(zoo);
+
+console.log(animals);
+
+// Using the zoo that you created above, print the value of the first item in
+// the Object
+
+console.log(zoo.lions);
+
+// Add an animal to the zoo Object and print the updated Object.
+zoo.sloths = 55
+console.log(zoo);
