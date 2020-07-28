eea645d1fa553c1b96f9c81513b869e35b373bc8
Whitney Wilkes <wilkes.whit@gmail.com>
2020-07-28 01:26:24 -0600

    Add day 6

--- day_6/exercises/dog.js
+++ day_6/exercises/dog.js
@@ -1,7 +1,7 @@
 /*
 In the exercises below, write your own code where indicated
 to achieve the desired result. You should be able to run this
-file from your terminal with the command `node day_4/exercises/dog.js`
+file from your terminal with the command `node day_6/exercises/dog.js`
 
 In the dog class below, add a play method that, when called, will result in
 the dog being hungry. Call that method below the class, and print the dog's
@@ -14,7 +14,6 @@ class Dog {
     this.name = name;
     this.age = age;
     this.hungry = true;
-
   }
 
   bark() {
@@ -31,17 +30,17 @@ class Dog {
 }
 
 var fido = new Dog("Bernese", "Fido", 4)
-
 console.log(fido.breed);
+//prints Bernese
 console.log(fido.name);
+//prints Fido
 console.log(fido.age);
+//prints 4
 console.log(fido.hungry);
-
-
+//prints true
 fido.eat();
-console.log(fido.hungry)
-
+console.log(fido.hungry);
+//prints false
 fido.play();
 console.log(fido.hungry);
-
-console.log(fido);
+//prints true, signifying that Fido does indeed get hungry when he plays! 
