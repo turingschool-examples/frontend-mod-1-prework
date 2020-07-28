2b487e1adee0ce38d0906e7d3b3582d2496d984c
Elle Li <yiran.li@hotmail.com>
2020-07-27 18:58:22 -0600

    Add Extension 1

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -1,7 +1,19 @@
+/*
 for (var i =10; i >= 1; i--) {
   if (i==1) {
     console.log( i +" speckled frog sat on a log eating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there were no more speckled frogs!");
   } else {
     console.log( i +" speckled frogs sat on a log eating some most delicious bugs.\nOne jumped in the pool where its nice and cool, then there were " + (i-1) + " speckled frogs.\n");
+  }
+}
+*/
+
+
+var frogs =["Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two","One"]
+for (var i = 0; i < frogs.length; i++) {
+  if (frogs[i]!="One") {
+    console.log(frogs[i] + "  speckled frogs sat on a log eating some most delicious bugs.\nOne jumped in the pool where its nice and cool, then there were " + (frogs[i+1]) + " speckled frogs.\n")
+  } else {
+  console.log( frogs[9] +" speckled frog sat on a log eating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there were no more speckled frogs!");
   }
 }
