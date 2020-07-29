955cae6f6a83011e40ce8de277242b5e9036b806
Whitney Wilkes <wilkes.whit@gmail.com>
2020-07-29 12:15:53 -0600

    Add day 7

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -34,3 +34,22 @@ the first verse in the above example would print 'Three speckled frogs...' and '
 
 /*Extension 2
 Make your program work for any number of frogs.*/
+var num = 100
+for (i = num; i >= 1; i--){
+
+if (i <= 1){
+
+console.log(i + " 1 speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!");
+
+} else if (i <= 2){
+
+  console.log(i + " 2 speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was 1 speckled frogs.");
+}
+
+else{
+
+  console.log(i + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were " + (i-1) + " speckled frogs.");
+
+}
+
+}
