7789f59a83551f04429cd1f851e3f8f9877534ee
Bryce Mara Jarrett <brycej98@gmail.com>
2020-07-28 21:08:37 -0500

    Add day 7

--- day_7/checker_board.js
+++ day_7/checker_board.js
@@ -1,4 +1,6 @@
 ///MY BOARD.
+// I actually watched a youtube video on this assignment and that's how I was able to wrtie this code
+// so not completely my work, had lots of googling help.
 
 var size = 8;
 var board = "";
@@ -16,4 +18,6 @@ for (let i = 1; i <= size; i++) {
   board += "\n";
 }
 console.log(board);
+
+
 ////
