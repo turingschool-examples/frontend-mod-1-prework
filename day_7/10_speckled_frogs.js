1654eb39936da94c997074b8379598620a7d2bb6
Whitney <65980084+whitneywilkes@users.noreply.github.com>
2020-07-29 10:49:38 -0600

    Update 10_speckled_frogs.js

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -1,4 +1,4 @@
-/*Create a file named 10_speckled_frogs.js and within that file, write several a program that will print the following nursery rhyme:
+*Create a file named 10_speckled_frogs.js and within that file, write several a program that will print the following nursery rhyme:
 
 3 speckled frogs sat on a log eating some most delicious bugs.
 One jumped in the pool where its nice and cool, then there were 2 speckled frogs.
@@ -12,19 +12,27 @@ One jumped in the pool where its nice and cool,
 then there were no more speckled frogs!
 
 Required
-Make your program print the rhyme above for 10 frogs, with attention to where language changes.*/
+Make your program print the rhyme above for 10 frogs, with attention to where language changes.
 
 var num = 10
 
 for (i = num; i >= 10; i--){
+  
 if (i <= 1){
+  
 console.log(i + " 1 speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!");
+
 } else if (i <= 2){
+  
   console.log(i + " 2 speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was 1 speckled frogs.");
 }
+  
 else{
+  
   console.log(i + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were " + (i-1) + " speckled frogs.");
+
 }
+  
 }
 
 /*Extension 1
