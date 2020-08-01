0a53a4a109cc4544312a719620ce9d14246b6fdf
Bruce Gordon <brc.gordon@gmail.com>
2020-08-01 16:59:51 -0600

    Complete encoding function for cipher

--- day_7/ceasar_cipher.js
+++ day_7/ceasar_cipher.js
@@ -1,11 +1,24 @@
-var msg = "Hello World"
-var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-splitMsg = msg.split("");
-console.log(splitMsg);
+//var msg = "Hello World"
 
 
+//console.log(splitMsg);
+function encodeMessage(msg, key) {
+  var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var capMsg = msg.toUpperCase();
+  var splitMsg = capMsg.split("");
+  var code = [];
+  for (var i = 0; i < splitMsg.length; i++) {
+    var output = alpha.indexOf(splitMsg[i]) - [key];
+     if (output === (-1 - key)) {
+       code.push(" ")
+     } else if (output < 0) {
+       code.push(alpha[output + 26])
+     } else {
+       code.push(alpha[output]);
+    }
+  }
+console.log(code.join(""))
 
+}
 
-
-//
+encodeMessage("Hello World", 5)
