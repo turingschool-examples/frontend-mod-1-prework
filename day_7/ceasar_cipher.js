28d614008daea7843b3c8b5f9ae95a0835e9d51a
Bruce Gordon <brc.gordon@gmail.com>
2020-08-01 14:49:26 -0600

    Split msg string into characters

---
+++ day_7/ceasar_cipher.js
@@ -0,0 +1,11 @@
+var msg = "Hello World"
+var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+splitMsg = msg.split("");
+console.log(splitMsg);
+
+
+
+
+
+//
