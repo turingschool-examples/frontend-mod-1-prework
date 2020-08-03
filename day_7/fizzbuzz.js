10344ea7b04f777a583f7df937e5cb6effaa8a99
Elle Li <yiran.li@hotmail.com>
2020-08-02 19:26:18 -0600

    Removing Number if Fizz,Buzz, and FizzBuzz

--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -1,12 +1,12 @@
 for (var i=1; i<= 100; i++){
   if (i % 3 ==0 && i % 5 ==0){
-    console.log(i + "FizzBuzz");
+    console.log("FizzBuzz");
   }
   else if (i % 3 == 0){
-    console.log(i + "Fizz");
+    console.log("Fizz");
   }
   else if (i % 5 == 0){
-    console.log(i + "Buzz");
+    console.log("Buzz");
   }
   else{
     console.log(i);
