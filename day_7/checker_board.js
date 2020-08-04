50ab4f690be34cd9fee2dff55b89d0cb55df00e6
Alyssa Bull <alyssabull@gmail.com>
2020-08-04 10:00:33 -0600

    Modify iteration for speckled frogs and checker board

--- day_7/checker_board.js
+++ day_7/checker_board.js
@@ -1,18 +1,19 @@
 class checkerBoard {
-  constructor (boardsize){
-  this.boardsize = boardsize;
+  constructor (boardWidth, boardLength){
+  this.boardWidth = boardWidth;
+  this.boardLength = boardLength;
 }
 create(){
-  if(this.boardsize < 1){
+  if(this.boardWidth < 1){
     console.log ("Error: board size must be a value greater than 1")
   }
-  else if (this.boardsize %1 !==0){
+  else if (this.boardWidth %1 !==0){
     console.log("Error: board size can only be created using whole numbers")
   }
   else{
-    for (var i = 1; i <=this.boardsize; i++) {
-      var oddBoardLine = "  X  X  X";
-      var evenBoardLine = "X  X  X";
+    for (var i = 1; i <=this.boardWidth; i++) {
+      var oddBoardLine = "  X";
+      var evenBoardLine = "X  ";
       if (i % 2 !== 0){
         console.log(oddBoardLine);
       }
@@ -28,5 +29,5 @@ var game1 = new checkerBoard (-1);
 game1.create();
 var game2 = new checkerBoard (6.5);
 game2.create();
-var game3 = new checkerBoard (8);
+var game3 = new checkerBoard (8,8);
 game3.create();
