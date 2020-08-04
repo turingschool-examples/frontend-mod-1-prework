140b20c4ef719825a79b98144cca996852483d0d
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-04 15:44:19 -0500

    Make new changes part 2

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -10,10 +10,10 @@ for (var i=3; i<11; i++) {
 
 //Counting down the speckled frogs
 
-for (var i=10; i>3; i--) {
+for (var i=10; i>2; i--) {
   console.log( i + " speckled frogs sat on a log \n\ eating some most delicious bugs. \n\ One jumped in a pool where it was nice and cool \n\ then there were " + (i-1) + " speckled frogs.");
 }
-for (var i=3; i>1; i--) {
+for (var i=2; i>1; i--) {
   console.log( i + " speckled frogs sat on a log \n\ eating some most delicious bugs. \n\ One jumped in a pool where it was nice and cool \n\ then there was 1 speckled frog.")
 }
 for (var i=1; i>0; i--) {
