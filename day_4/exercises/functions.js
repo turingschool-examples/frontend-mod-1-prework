df6ca9788d1cc4891c4969da576d55611ca5d66f
Ismail Hartley <Ismailhartley1@gmail.com>
2020-08-04 11:13:45 -0600

    first edit to day 4 work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,7 +5,7 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
+function printName(name) {
   console.log("Severus Snape");
 };
 
@@ -14,36 +14,28 @@ printName();
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() {
+function printName(name) {
   console.log("Ismail Hartley");
 };
 
-printName();
+printName(Ismail);
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
 
-var ourNumber = 2
-
-var theirNumber = 3
-
-function addNumber() {
+function addNumber(ourNumber, theirNumber) {
   console.log(ourNumber + theirNumber);
 };
 
-addNumber();
-
-
+addNumber(2, 3);
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-var Ish = "Ish really enjoys fishing,"
 
-var Karim = "Karim does as well."
 
-function ishKarim() {
-  console.log(Ish  +  Karim);
+function bestFriends(friend1, friend2) {
+  console.log(friend1  + 'and' + friend2 + 'are best friends');
 };
 
-ishKarim();
+bestFriends(Ish, Karim);
