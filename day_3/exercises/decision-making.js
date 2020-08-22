0c8b68cd4ca957d9b01887c8fd5317f9482e51a4
Boone Epstein <booneepstein@gmail.com>
2020-08-21 22:22:06 -0500

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -40,16 +40,24 @@ if (bearChoice === 1) {
 /*
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
+Depending on which door you enter the bear will putting on a different item of clothing. In lines 17-21 an if statment evaluates a variable reflecting that choice, and assigns a new value to the variable bearClothing based on the result of that evaluation.
 
 2. What variable has a new value assigned to it after the first if statement executes?
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+"scarf"
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+The story has three endings based on what choice you make. This choice is represented by the value of the variable bearChoice. Lines 31-38 are an if statement in which each potential value of bearChoice is evaluated, and if a specific evaluation is true it will log the corresponding ending to the console.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+"You run as fast as you can into the next room. It's full of snakes!"
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+"You offer the bear your hat and the bear shows you a secret passage out!"
 
 7. What is your favorite ending?
+"You stay with the bear and become it's best friend!"
+
 */
