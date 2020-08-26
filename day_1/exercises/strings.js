e4c8a96d563db4779744c070626d40a1399fece2
Tashia Davis <tashiadavis10@gmail.com>
2020-08-26 15:33:25 -0600

    Add day 1

--- day_1/exercises/strings.js
+++ day_1/exercises/strings.js
@@ -5,13 +5,13 @@ file from your terminal with the command `node day_1/exercises/strings.js`
 */
 
 // example: Write code below to log your name in the console:
-console.log("Alan Turing");
+console.log("Tashia Davis");
 
 // Write code below to log `Welcome to Turing!` in the console:
-console.log(/*YOUR CODE HERE*/);
+console.log("Welcome to Turing!");
 
 // Write code below to log `99 bottles of pop on the wall...`:
-// YOUR CODE HERE
+console.log("99 bottles of pop on the wall...");
 
 //Write code below to log one line from your favorite song or movie:
-// YOUR CODE HERE
+console.log("This must be the place.");
