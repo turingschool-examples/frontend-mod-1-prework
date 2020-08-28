4ce4c3813bc43b672ba3367e7ee0de3cfdafc0be
Drew Bradley <williamandrewb@gmail.com>
2020-08-28 13:09:20 -0600

    update arrays.js to include solutions

--- day_2/exercises/array.js
+++ day_2/exercises/array.js
@@ -10,7 +10,7 @@ var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals);
 
 // Declare a variable that will store an an array of at least 3 foods (strings)
-// YOUR CODE HERE
+var foods = ['banana', 'orange', 'apple', 'cherry']
 
 // example: Write code below that will log the number of elements in array of
 // animals from above.
@@ -18,28 +18,37 @@ console.log(animals.length);
 
 // Write code below that will log the number of elements in the array of
 // foods from above.
-// YOUR CODE HERE
+console.log(foods.length);
 
 // Write code below that will log "Zebra" from the animals array
-// YOUR CODE HERE
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the first item in the animals array
-// YOUR CODE HERE
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the last item in the animals array
-// YOUR CODE HERE
+console.log(animals[2]);
 
 // Write code using bracket notation that will reassign the last item in the animals
 // array to "Gorilla"
-// YOUR CODE HERE
+animals.pop();
+animals.push('Gorilla');
+console.log(animals);
+//or
+animals.splice(2, 1, 'Gorilla');
+console.log(animals);
 
 // Write code below that will log the last item from the foods array.
-// YOUR CODE HERE
+console.log(foods[3]);
+// or if the length is unknown, you could do this:
+console.log(foods[foods.length - 1]);
 
 // Write code below that uses a method to add "lion" to the animals array and
 // log the changed array to verify "lion" has been added
-// YOUR CODE HERE
+animals.push('Lion');
+console.log(animals);
 
 // Write code below that removes the last item of food from the foods array and
 // log the cahnged array to verify that item has been removed
-// YOUR CODE HERE
+foods.push('double bacon cheeseburger with extra pickles');
+console.log(foods);
