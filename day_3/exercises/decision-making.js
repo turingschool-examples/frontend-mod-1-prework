4de73c22c8ba7ea956b894b34a47d20d2fef89ba
Cameron Aragon <caragon4695@gmail.com>
2020-08-30 12:16:04 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -10,7 +10,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 2;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -41,15 +41,29 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+It's saying that if the door choice is equal by data type and value to door choice 1, then the bear clothing will be a hat. If the door choice is anything else other than 1, the bear will be wearing a scarf.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+The `bearClothing` variable has a new value assigned to it after the first if statement executes.
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+If `doorChoice` was changed to equal 3 the `bearClothing` would still be a scarf.
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+
+If the bear choice is completely equal to 1 then it will print a specific statement inclding the bear clothing option stated previously. If the bear choice is completely equal to 2 it will print another new statement that also includes the bear clothing option. If the bear choice is equal to 3 then it will print a statement that doesn't include the bear clothing option. If anything else is the bear choice value then it will print a statement that also doesn't include the bear clothing in it.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+If the bearChoice varaible equals 3 then the "You run as fast as you can into the next room. It's full of snakes!" statement will be printed.
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+With those selected variables the final outcome will be "You tell the bear the hat is too small and it starts to cry!".
+
 7. What is your favorite ending?
+
+My favorite ending is " You offer the bear your scarf and the bear shows you a secret passage out!".
+
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,30 @@
 
 1. What is a conditional statement? Give one example of a daily life conditional. Give one example of where a conditional is probably used in a web application you use.
 
+A conditional statement is based on on a set of conditions using if/then/else. It says what to do based on a given situation. One example would be when I'm entering a username and password with a website, it'll either be correct and let me through or incorrect and make me re-enter my credentials.
+
 2. Why might you want to use an `if` statement?
 
-3. What is the JavaScript syntax for an `if` statement?
+One would want to use an `if` statement when they're trying to evaluate/check a condition.
 
+3. What is the JavaScript syntax for an `if` statement?
+```
+if (condition) {
+  // block of code to be executed if evaluated to true
+}
+```
 4. How do you add multiple conditions to an `if` statement?
 
-5. What is the JavaScript syntax for an `if/else` statement?
+If wanting to add more conditions to an `if` statement, the `if/else` statement should be used.
 
+5. What is the JavaScript syntax for an `if/else` statement?
+```
+if (condition) {
+  // block of code to be executed if condition evaluates true
+} else {
+  // block of code to be executed if condition evaluates false
+}
+```
 6. Other than an `if` statement, can you think of any other ways we might want to use a conditional statement?
+
+Another way to use a conditional statement is a `switch` statement.
