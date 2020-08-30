1156875ad31e634397ec44a6febaa84ef638d8e7
Cameron Aragon <caragon4695@gmail.com>
2020-08-29 20:31:12 -0600

    Add Day 2 Work

---
+++ day_2/array_methods.md
@@ -0,0 +1,7 @@
+The `pop` array method will remove an item from the end of an array.
+
+The `push` array method will add an item to the end of an array.
+
+The `shift` array method will remove an item from the beginning of an array.
+
+The `unshift` array method will add an item to the beginning of an array.
--- day_2/exercises/iteration.js
+++ day_2/exercises/iteration.js
@@ -16,13 +16,20 @@ for (var i = 0; i < animals.length; i++) {
 // <animal> is awesome!" for each animal.
 
 for (var i = 0; i < animals.length; i++) {
-  // YOUR CODE HERE
+  console.log("The " + animals[i] + " is awesome!");
 }
 
 // Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
-// YOUR CODE HERE
+var foods = ["Broccoli", "Kale", "Tofu", "Olive Oil"]
+
+for (var i = 0; i < foods.length; i++) {
+  console.log("Add " + foods[i] + " to shopping list");
+}
 
 // Create an array of numbers and then iterate over that array to log doubles
 // of each of the number.
-// YOUR CODE HERE
+var numbers = [4, 14, 25, 11, 7];
+numbers.push(...numbers);
+
+  console.log(numbers);
