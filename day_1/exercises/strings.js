32f7b16055ea7644e773fb2637a8482992387f3f
Cole Fiscus <coletfiscus@gmail.com>
2020-08-30 20:48:42 -0600

    Add day 1

--- day_1/exercises/strings.js
+++ day_1/exercises/strings.js
@@ -5,13 +5,13 @@ file from your terminal with the command `node day_1/exercises/strings.js`
 */
 
 // example: Write code below to log your name in the console:
-console.log("Alan Turing");
+console.log("Cole Fiscus");
 
 // Write code below to log `Welcome to Turing!` in the console:
-console.log(/*YOUR CODE HERE*/);
+console.log("Welcome to Turing!");
 
 // Write code below to log `99 bottles of pop on the wall...`:
-// YOUR CODE HERE
+console.log("99 bottles of pop on the wall...");
 
 //Write code below to log one line from your favorite song or movie:
-// YOUR CODE HERE
+console.log("Home is only a feeling you get in your mind, from the people you love and you travel beside.");
