569c7581edb2936ecb38b20ab4670439fcd9dc07
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-08-31 14:14:09 -0600

    Add Say 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,37 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
+/*function printName() {
   console.log("Severus Snape");
 };
 
-printName();
+printName();*/
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName(functionName) { // hint.. add a parameter on this line :)
+  console.log(functionName)
 };
 
-printName("Albus Dumbledore");
+printName("Kevin Hartmann");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
 //YOUR CODE HERE
 
+function getSum(firstNumber, secondNumber){
+  console.log(firstNumber + secondNumber)
+};
+
+getSum(12, 11);
+
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-// YOUR CODE HERE
+
+function printSentence(nameOne, nameTwo){
+  console.log(nameOne + " and " + nameTwo + " " + "went on vaction to London!")
+};
+
+printSentence("Kevin", "Marian");
