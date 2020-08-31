1c57ea6cc3ed69cfdfdd7bb83045957c59360b06
Brian Forbes <brianandrewf81@gmail.com>
2020-08-31 17:24:38 -0600

    Add day 1

--- day_1/exercises/strings.js
+++ day_1/exercises/strings.js
@@ -8,10 +8,12 @@ file from your terminal with the command `node day_1/exercises/strings.js`
 console.log("Alan Turing");
 
 // Write code below to log `Welcome to Turing!` in the console:
-console.log(/*YOUR CODE HERE*/);
+console.log("Welcome to Turing");
 
 // Write code below to log `99 bottles of pop on the wall...`:
+console.log("99 bottles of pop on the wall...")
 // YOUR CODE HERE
 
 //Write code below to log one line from your favorite song or movie:
+console.log("Where we're going, we don't need roads")
 // YOUR CODE HERE
