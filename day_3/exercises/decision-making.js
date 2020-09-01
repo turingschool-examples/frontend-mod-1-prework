7857284f56e4b62fa063fa0adaa6b4e43665bee3
Christina Cook <cmc.cook7@gmail.com>
2020-09-01 11:46:23 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -41,15 +41,43 @@ if (bearChoice === 1) {
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
 
+This if-statement is evaluating the different outcomes of bearCLothing.
+If doorChoice is equal to 1 evaluates as true, then the value of bearClothing is "hat".
+If this statement returns as false, then the value of bearCLothing is "scarf".
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+The value of bearCLothing would be "scarf".
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+
+Here, we are using else if statement to evaluate mutliple conditions.
+If bearChoice equals 1 returns true, the subsequent block of code is executed.
+If bearChoide equals 1 returns false, the program moves on to else if (bearChoice === 2).
+If this condition returns true, the subsequent block of code is executed.
+But if this condition returns false, the program moves on to else if (bearClothing === 3).
+Again, if this condition is true, the subsequent block of code is executed.
+But if it returns false, then the program moves on to else (line 36) and returns the code block that follows (meaning none of the conditions were true).
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+When I ran this code, the final outcome was "You stay with the bear and become it's best friend!"
+Initially I was confused, thinking line 34 listed the condition (bearChoice === 3),
+so I thought the outcome would be "You run as fast as you can into the next room. It's full of snakes!"
+But then figured out line 34 actually said (bearClothing === 3).
+Not sure if this was a typo or not, but based on the code given, none of the conditions returned true, so the last block of code was run.
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+Based on the order of the previous questions, the final outcome is: "You tell the bear the scarf is too small and it starts to cry!"
+But if you go back and run the first if-statement again, and rerun the else if statement, the value of bearCLothing changes to "hat",
+making the final outcome "You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+
+My favorite ending is: "You stay with the bear and become it's best friend!"
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -2,12 +2,37 @@
 
 1. What is a conditional statement? Give one example of a daily life conditional. Give one example of where a conditional is probably used in a web application you use.
 
+A conditional statement tells the program what to do in a given situation. Daily life conditional example: if I'm hungry in the afternoon, I eat a snack, otherwise, I don't eat a snack. A conditional is probably used in Slack when messages are sent: if someone receives a message, an alert sound is made, otherwise nothing happens.
+
 2. Why might you want to use an `if` statement?
+
+If statements are used to evaluate conditions and decide what code to run. This could be done to display results of a quiz someone takes online.
 
 3. What is the JavaScript syntax for an `if` statement?
 
+```JavaScript
+if (condition) {
+  console.log(); //block of code to execute if condition is true
+}
+```
 4. How do you add multiple conditions to an `if` statement?
+
+You add multiple conditions with an 'else if' statement.
 
 5. What is the JavaScript syntax for an `if/else` statement?
 
+```JavaScript
+if (condition1) {
+  console.log(); //block of code to execute if condition1 is true
+} else if (condition2) {
+  console.log(); //block of code to execute if condition1 is false and condition2 is true
+} else if (condition3) {
+  console.log(); //block of code to execute if condition1 and condition2 are false and condition3 is true
+} else {
+  console.log(); //block of code to execute if none of the above conditions are true
+}
+```
+
 6. Other than an `if` statement, can you think of any other ways we might want to use a conditional statement?
+
+Rather than running through all the code and checking each 'if' statement, you could use a 'switch' statement which speeds up the performance with the use of 'break' statements.
