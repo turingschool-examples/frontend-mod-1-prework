f9832b15291af5b7e5226dc69cc4eb0ca5b14f5a
Lucas Merchant <lbmerchant93@gmail.com>
2020-09-02 13:22:00 -0600

    Add day 2 work

--- day_2/exercises/array.js
+++ day_2/exercises/array.js
@@ -10,7 +10,7 @@ var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals);
 
 // Declare a variable that will store an an array of at least 3 foods (strings)
-// YOUR CODE HERE
+var foods = ["Quinoa", "Lentils", "Rice"];
 
 // example: Write code below that will log the number of elements in array of
 // animals from above.
@@ -18,28 +18,33 @@ console.log(animals.length);
 
 // Write code below that will log the number of elements in the array of
 // foods from above.
-// YOUR CODE HERE
+console.log(foods.length);
 
 // Write code below that will log "Zebra" from the animals array
-// YOUR CODE HERE
+console.log(animals[0]);
 
 // Write code using bracket notation that will log the first item in the animals array
-// YOUR CODE HERE
+let first = animals[0];
+console.log(first);
 
 // Write code using bracket notation that will log the last item in the animals array
-// YOUR CODE HERE
+let last = animals[2];
+console.log(last);
 
 // Write code using bracket notation that will reassign the last item in the animals
 // array to "Gorilla"
-// YOUR CODE HERE
+animals[2] = Gorilla;
 
 // Write code below that will log the last item from the foods array.
-// YOUR CODE HERE
+console.log(foods[2]);
 
 // Write code below that uses a method to add "lion" to the animals array and
 // log the changed array to verify "lion" has been added
-// YOUR CODE HERE
+let newAnimals = animals.push("Lion");
+console.log(animals);
+
 
 // Write code below that removes the last item of food from the foods array and
 // log the cahnged array to verify that item has been removed
-// YOUR CODE HERE
+let last = foods.pop();
+console.log(foods);
--- day_2/questions.md
+++ day_2/questions.md
@@ -2,14 +2,34 @@
 
 1. Declare a variable named `animals` that stores an array containing the following strings: `"zebra", "giraffe", "elephant"`.
 
+var animals = ["zebra", "giraffe", "elephant"];
+
 2. Using the array `animals`, how would you access `"giraffe"`?
+
+console.log(animals[1]);
 
 3. How would you add `"lion"` to the `animals` array?
 
+let newAnimal = animals.push("lion");
+
 4. Name and describe two additional array methods.
+
+* To remove an item from the beginning of an array:
+let firstAnimalGo = animals.shift();    //removing "zebra" from the  array
+
+* To add an item to the beginning of an array:
+let newFirstAnimal = animals.unshift("dog");    //adding "dog" as the first in the array
 
 5. What are the boolean values in JavaScript?
 
+Everything in a boolean is either 'true' or 'false'.
+Everything without a "value" is false:
+values of 0, -0, "" (empty string), undefined, null, false, NaN
+
 6. In JavaScript, how would you evaluate if `2` is equal to `25`? What is the result of this evaluation?
 
+console.log(2 == 25); //false
+
 7. In JavaScript, how would you evaluate if `25` is greater than `2`? What is the result of this evaluation?
+
+console.log(25 > 2); //true
