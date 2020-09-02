57bdeeb7031983087a8016d95a962d287e430e6b
Drew Bradley <williamandrewb@gmail.com>
2020-09-02 09:26:55 -0600

    Update objects.js

--- day_5/exercises/objects.js
+++ day_5/exercises/objects.js
@@ -15,18 +15,26 @@ console.log(foods);
 
 // Write code below that will print an B=Object of animals and the number
 // of that type of animal at the zoo. (an inventory of animals)
-var zoo = // YOUR CODE HERE
+var zoo = {
+  zebras: 6,
+  giraffes: 5,
+  elephants: 8,
+  monkeys: 12,
+  goats: 9,
+  bees: 500
+};
 console.log(zoo)
 
 // Using the zoo that you created above, print all the keys in the Object.
-// YOUR CODE HERE
+console.log(Object.keys(zoo));
 
 // Using the zoo that you created above, print all the values in the Object.
-// YOUR CODE here
+console.log(Object.values(zoo));
 
 // Using the zoo that you created above, print the value of the first item in
 // the Object
-// YOUR CODE HERE
+console.log(zoo.zebras)
 
 // Add an animal to the zoo Object and print the updated Object.
-// YOUR CODE HERE
+zoo.cats = 32
+console.log(zoo);
