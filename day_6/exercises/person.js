0c7ff6fa9fc4a20cffaf007560faf7c39db90c49
Rachel Buchta <rachelbuchta@yahoo.com>
2020-09-05 16:42:47 -0600

    Add Day 6 Work

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,4 +8,28 @@ person methods below the class so that they print their result to the
 terminal.
 */
 
-// YOUR CODE HERE
+class Person {
+  constructor(name, age, height, weight, hungry, poundsLost){
+  this.name = name;
+  this.age = age;
+  this.height = height;
+  this.weight = weight;
+  this.hungry = true;
+  this.poundsLost = poundsLost;
+  }
+
+  eatDinner(){
+    this.hungry = false;
+  }
+
+  loseWeight(weight, poundsLost){
+    this.weight = this.weight - this.poundsLost;
+  }
+}
+
+var person1 = new Person("Stefan", "26", "6 ft", "185", "true", "5");
+person1.eatDinner();
+console.log(person1.eatDinner);
+person1.loseWeight(185 - 5);
+console.log(person1.loseWeight);
+console.log(person1);
