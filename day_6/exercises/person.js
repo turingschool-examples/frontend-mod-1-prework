b4cb9cf852048b74ee11eed8cb9679a768041a1b
Christina Cook <cmc.cook7@gmail.com>
2020-09-05 12:46:50 -0600

    Add day 6 work

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -9,3 +9,49 @@ terminal.
 */
 
 // YOUR CODE HERE
+class Person {
+  constructor(name, age, location, friends) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.friends = friends
+    this.tired = false;
+  }
+
+  moveCity(location) {
+    this.location = location;
+  }
+
+  makeNewFriend(newFriend) {
+    this.friends.push(newFriend);
+  }
+
+  runMarathon() {
+    this.tired = true;
+  }
+}
+
+var christina = new Person("Christina", 32, "Denver", ["Ashley", "Stacy", "Katie", "Amanda"]);
+
+console.log(christina);
+/*
+Person {
+  name: 'Christina',
+  age: 32,
+  location: 'Denver',
+  friends: [ 'Ashley', 'Stacy', 'Katie', 'Amanda' ],
+  tired: false
+}
+*/
+
+christina.makeNewFriend("Sam");
+console.log(christina.friends);
+// [ 'Ashley', 'Stacy', 'Katie', 'Amanda', 'Sam' ]
+
+christina.moveCity("Las Vegas");
+console.log(christina.location);
+// Las Vegas
+
+christina.runMarathon();
+console.log(christina.tired);
+// true
