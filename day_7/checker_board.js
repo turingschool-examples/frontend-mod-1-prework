01ae5d49e7758ec21aac597d06e8d5a2ac321453
Drew Bradley <williamandrewb@gmail.com>
2020-09-07 17:12:59 -0600

    Update checker_board.js

---
+++ day_7/checker_board.js
@@ -0,0 +1,7 @@
+/*
+Create a file called `checker_board.js` and within that file, write a program that will log a checkerboard based on the size *indicated by the user*.  On this board, the black spaces will be represented with 'X' and the white spaces will be represented with ' '. And example of the output for a size 6 board would look like this:
+*/
+
+function checkerboard(x, y) {
+    
+}
