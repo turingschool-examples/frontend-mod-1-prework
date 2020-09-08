9f54817f775d338e6422075b5aea3a768b792f52
Drew Bradley <williamandrewb@gmail.com>
2020-09-08 08:57:49 -0600

    Update checker_board.js and complete high_level.md

--- day_7/checker_board.js
+++ day_7/checker_board.js
@@ -7,5 +7,36 @@ Divide the value of 'y' by two, since characters are two digits long.
 */
 
 function checkerboard(x, y) {
-    
+    var result = '';
+    for (var i = 0; i < x; i++){
+        for(var j = 0; j < y; j++){
+            if( (i%2 === 0 && j%2 === 0) || (i%2 === 1 && j%2 === 1) ) {
+                result += ' ';
+            }else if ( (i%2 !== 0 && j%2 === 0) || (i%2 === 0 && j%2 !== 0)) {
+                result += '#';
+              }
+        }
+    }
+    console.log(result);
 }
+
+checkerboard(5, 10);
+
+
+/*
+let size = 8, result= '';
+for(let i=0; i < size; i++) { 
+  for(let j=0; j < size; j++) {
+      if(i !== 0 && j === 0 ) {
+      result += '\n';
+      }
+      if( (i%2 === 0 && j%2 === 0) || (i%2 ===1 && j%2 === 1)) {
+       result += ' ';
+      } else if( (i%2 !== 0 && j%2 === 0) || (i%2 === 0 && j%2 !== 0)) {
+        result += '#';
+      }
+  }
+}
+
+console.log(result);
+*/
