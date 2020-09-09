cfd8ea6e8c29efa62afabcf04e801147e03aee5e
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-09-09 19:55:57 -0400

    Update on highlevel project

--- day_7/highlevel/caesar_cipher.js
+++ day_7/highlevel/caesar_cipher.js
@@ -2,8 +2,6 @@ class Cipher {
     constructor(str, num) {
         this.str = str;
         this.num = num;
-
-
     }
 
     mystery(str, num) {
@@ -19,7 +17,7 @@ class Cipher {
                 encodeStr += currentLetter;
                 continue;
             }
-
+// should I use the array.prototype.replace(x,y) somewhere? 
             var currentIndex = alphabet.indexOf(currentLetter);
 
             var encodedIndex = currentIndex + this.num;
@@ -40,7 +38,11 @@ class Cipher {
     }
 }
 
-var myCipher = new Cipher("This is a test.", 2);
+var myCipher = new Cipher("This is a test", 2);
 console.log(myCipher);
 
 myCipher.mystery();
+
+// I was able to create an instance of this class, but struggled completing the method. I tried mutliple ways, watched Youtube, googled. I would try to isolate each portion with a simplified version in attempts to see what needed to be changed or improved upon. I have tried new techniques that I'm not fully confident that I understand. I tried using the %26 as a way instead to shift the characters, but settled on this for now. 
+// I felt it important to document my struggle for my own acknowledgment. 
+// Tomorrow is passed the deadline, but tomorrow is another chance for me to struggle again. 
---
+++ day_7/highlevel/highlevel.md
@@ -0,0 +1,12 @@
+## Ceasar Cipher
+
+1. Based on the parameter that the js program needs to respond to any string and shift value entered by a user, I need to create an object. 
+* The user's interaction will thus be considered an instance of the object.
+2. I will create a class with the following constructors of str and num so the user might create their instance. 
+3. Then, a method will be included that will iterate through the str.length and shift the letters used based on the userShift value. 
+* This includes first breaking the string into individual letters while ignoring spaces and iterating through each letter and shifting.
+* Then once it's shifted, the array needs to reform into a single string. 
+* The necessary letter's must be capitalized.
+* The necessary spaces need to be included to maintain meaning. 
+
+
