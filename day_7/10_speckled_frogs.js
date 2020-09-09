e9cbd3c2bae715d89ee3ce51eccaaff0311b7bcc
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-09-09 12:31:01 -0600

    Add Day 7 Work

---
+++ day_7/10_speckled_frogs.js
@@ -0,0 +1,7 @@
+for (let i = 10; i > 2; i--){
+  console.log(i + " speckled frogs sat on a log \neating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there were " + (i - 1) + " speckled frogs.\n")
+}if(i = 2){
+  console.log(i + " speckled frogs sat on a log \neating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there was " + (i - 1) + " speckled frog.\n")
+}if(i = 1){
+  console.log(i + " speckled frog sat on a log \neating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there were no more speckled frogs!\n")
+};
