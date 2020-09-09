792d06ff4ea0ef3167cc33bdbb7a064376351bd4
mattdean <matthewallandean@gmail.com>
2020-09-08 21:23:10 -0600

    Finish Day 7 Work

--- day_7/10_speckled_frogs.js
+++ day_7/10_speckled_frogs.js
@@ -1,4 +1,8 @@
-for (var i = 10; i >= 1; i--) {
+//Required
+
+function howManyFrogs(x)
+
+for (var i = 10; i >= x; i--) {
   if (i == 2) {
     console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there was " + (i-1) + " speckled frogs.");
   } else if (i == 1) {
@@ -7,3 +11,24 @@ for (var i = 10; i >= 1; i--) {
     console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were " + (i-1) + " speckled frogs.");
 }
 };
+
+
+//Extension 1
+
+
+//Extension 2
+
+function howManyFrogs(x) {
+
+for (var i = x; i >= 1; i--) {
+  if (i == 2) {
+    console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there was " + (i-1) + " speckled frogs.");
+  } else if (i == 1) {
+    console.log(i + " speckled frog sat on a log eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were no more speckled frogs.")
+  } else {
+    console.log(i + " speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were " + (i-1) + " speckled frogs.");
+     }
+  }
+};
+
+howManyFrogs(5);
--- day_7/fizzbuzz.js
+++ day_7/fizzbuzz.js
@@ -1,3 +1,4 @@
+// For values 1-100
 for (var i = 1; i <= 100; i++)
 
 if (i%3 == 0 && i%5 == 0) {
@@ -10,4 +11,22 @@ if (i%3 == 0 && i%5 == 0) {
     console.log(i)
 };
 
-// I had the && operator set as the last else if statement at first, realized those numbers were already output as "Fizz".
+
+//Bonus:  For any number run fizzBuzzer(x)
+
+function fizzBuzzer(x) {
+
+for (var i = 1; i <= x; i++)
+
+if (i%3 == 0 && i%5 == 0) {
+  console.log("FizzBuzz")
+} else if (i%3 == 0) {
+  console.log("Fizz")
+} else if (i%5 == 0) {
+  console.log("Buzz")
+} else {
+    console.log(i)
+}
+};
+
+fizzBuzzer(100)
