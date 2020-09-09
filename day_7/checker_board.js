45e7a95fb1e68b61e79147e8b3a64dd1adada034
mattdean <matthewallandean@gmail.com>
2020-09-08 18:56:04 -0600

    Rework checker_board,js challenge

--- day_7/checker_board.js
+++ day_7/checker_board.js
@@ -5,11 +5,13 @@ var evenNum = " X"
 for (var i = 1; i <= gridSize; i++)
   if (gridSize === 1){
     console.log('X');
-  }else if (gridSize % 2 !== 0) {
+  }else if (gridSize % 2 !== 0 && i % 2 !== 0) {
     console.log('X ' + oddNum.repeat((gridSize / 2) - 1) + 'X');
-  } else if (gridSize % 2 === 0) {
-    console.log(oddNum.repeat(gridSize / 2));
+  } else if (gridSize % 2 !== 0 && i % 2 ===0) {
+    console.log(" " + oddNum.repeat(gridSize / 2));
+  } else if (gridSize % 2 === 0 && i % 2 === 0) {
+    console.log(evenNum.repeat(gridSize / 2));
     } else {
-      console.log(evenNum.repeat(gridSize / 2));
+      console.log(oddNum.repeat(gridSize / 2));
     }
 };
