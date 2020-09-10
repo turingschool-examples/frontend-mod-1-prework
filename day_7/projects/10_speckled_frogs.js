1df1d450d2e9a7600eb1e252e49559056315e4d7
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-09-10 16:49:41 -0400

    Complete extensions 1 and 2

--- day_7/projects/10_speckled_frogs.js
+++ day_7/projects/10_speckled_frogs.js
@@ -68,4 +68,112 @@ for (var i = 10; i >= 1; i--) {
 // basic completed.
 
 // extension 1: print word versions rather than numerals. 
-// this would mean creating an array with each? Or is there a way I can create a loop that'll do it for me? 
+
+for (var i = 10; i >= 1; i--) {
+
+    var num1 = ["Two", "one"];
+    var num2 = ["one", "no more"];
+    var plural = ["frogs", "frog"];
+    var singular = ["frogs", "frog"];
+    var was = "was ";
+    var firstWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+    var secondWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    if (i >= 3) {
+        num1 = i;
+        num2 = i - 1;
+        plural = plural[0];
+        singular = singular[0];
+
+        num1 = firstWords[i - 1];
+        num1 = num1.charAt(0).toUpperCase() + num1.slice(1);
+
+        num2 = secondWords[i - 2];
+
+        console.log(num1 + " speckled " + plural + " sat on a log" +
+            " eating some most delicious bugs." +
+            " One jumped in the pool where its nice and cool," +
+            " then there were " + num2 + " speckled " + singular + ".");
+    }
+    else if (i == 2) {
+        num1 = num1[0];
+        num2 = num2[0];
+        plural = plural[0];
+        singular = singular[1];
+        console.log(num1 + " speckled " + plural + " sat on a log" +
+            " eating some most delicious bugs." +
+            " One jumped in the pool where its nice and cool,"+
+            " then there "+ was + num2 + " speckled " + singular + ".");
+    }
+    else if (i == 1) {
+        num1 = num1[1];
+        num2 = num2[1];
+        plural = plural[1];
+        singular = singular[0];
+        num1 = num1.charAt(0).toUpperCase() + num1.slice(1);
+
+        console.log(num1 + " speckled " + plural + " sat on a log" + 
+            " eating some most delicious bugs." +
+            " One jumped in the pool where its nice and cool," + 
+            " then there were " + num2 + " speckled " + singular + ".");
+    }
+
+}
+
+// extension 1 complete! Feeling very proud of my work on this! 
+
+//extension 2: how to make it work for any number of frogs? Should I worry about being able to translate the numbers into words or stick with the basics? Let's stick with the numerals first. I need to make a class and then a function which will allow for the instance and that transformation. 
+
+class speckledFrogs {
+
+    howManyFrogs(userFrogs) {
+        this.userFrogs = userFrogs
+
+        for (var i = this.userFrogs; i >= 1; i--) {
+
+            var num1 = ["2", "1"];
+            var num2 = ["1", "no more"];
+            var plural = ["frogs", "frog"];
+            var singular = ["frogs", "frog"];
+            var was = "was ";
+
+            if (i >= 3) {
+                num1 = i;
+                num2 = i - 1;
+                plural = plural[0];
+                singular = singular[0];
+                console.log(num1 + " speckled " + plural + " sat on a log" +
+                    " eating some most delicious bugs." +
+                    " One jumped in the pool where its nice and cool," +
+                    " then there were " + num2 + " speckled " + singular + ".");
+            }
+            else if (i == 2) {
+                num1 = num1[0];
+                num2 = num2[0];
+                plural = plural[0];
+                singular = singular[1];
+                console.log(num1 + " speckled " + plural + " sat on a log" +
+                    " eating some most delicious bugs." +
+                    " One jumped in the pool where its nice and cool," +
+                    " then there " + was + num2 + " speckled " + singular + ".");
+            }
+            else if (i == 1) {
+                num1 = num1[1];
+                num2 = num2[1];
+                plural = plural[1];
+                singular = singular[0];
+                console.log(num1 + " speckled " + plural + " sat on a log" +
+                    " eating some most delicious bugs." +
+                    " One jumped in the pool where its nice and cool," +
+                    " then there were " + num2 + " speckled " + singular + ".");
+            }
+
+        }
+    }
+
+}
+
+var myFrogs = new speckledFrogs();
+myFrogs.howManyFrogs(5);
+
+// I was right on my instinct! The extra-extra challenge is: how can I output word values for the numbers without creating a huge ass array because it's impossible to predict what someone would input. 
