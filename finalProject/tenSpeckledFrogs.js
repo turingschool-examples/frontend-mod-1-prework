56933cf1df15009bf9e2f27135492ee35daef002
Alice <aliceruppert@gmail.com>
2020-09-09 21:23:24 -0600

    Add frogs exercise

---
+++ finalProject/tenSpeckledFrogs.js
@@ -0,0 +1,30 @@
+/*
+console.log the nursery rhyme
+keep the line breaks
+have the number of stanzas be the number of frogs-1
+change number of frogs within
+last stanza is differnt ending line
+*/
+
+function frogRhyme(numberOfFrogs) {
+  let rhyme = ''
+  for (let i = numberOfFrogs; i > 0; i--) {
+    let number = i
+    let oneLessNumber = i - 1
+    let frogText = ' speckled frog sat on a log\neating some most delicious bugs.\nOne jumped in the pool where its nice and cool,\nthen there '
+
+    if (i >= 3) {
+      let stanza = number + frogText + 'were ' + oneLessNumber + ' speckled frogs!\n\n';
+      rhyme += stanza
+    } else if (i === 2) {
+      let secondLastStanza = number + frogText + 'was ' + oneLessNumber + ' speckled frogs!\n\n';
+      rhyme += secondLastStanza
+    } else if (i === 1) {
+      let lastStanza = number + frogText +  'were no more speckled frogs!';
+      rhyme += lastStanza
+    }
+  };
+  console.log(rhyme)
+};
+
+frogRhyme(10);
