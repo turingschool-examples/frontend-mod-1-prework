953f4d66e938aeb5edcf7cd7e6f8bb5d750c382d
Reggie Thompson <reggie.thompson4@gmail.com>
2020-10-10 10:54:04 -0600

    Add day 6 work

--- day_6/exercises/person.js
+++ day_6/exercises/person.js
@@ -8,4 +8,27 @@ person methods below the class so that they print their result to the
 terminal.
 */
 
-// YOUR CODE HERE
+class Person {
+  constructor (hairColor, favoriteBand, tired) {
+  this.hairColor = hairColor;
+  this.favoriteBand = favoriteBand;
+  this.tired = tired;
+    }
+  workOut() {
+    return this.tired = true;
+  }
+  sleep() {
+    return this.tired = false;
+  }
+}
+
+//Person.workOut();
+
+var reggie = new Person('black','Manchester Orchestra', false);
+
+console.log(reggie.hairColor);
+console.log(reggie.favoriteBand);
+reggie.workOut();
+console.log(reggie.tired);
+reggie.sleep();
+console.log(reggie.tired);
