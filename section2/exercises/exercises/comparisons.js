cf71a08c92ebaf7758f6627d3b0f081f84bcda4e
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-10-13 09:16:09 -0500

    Complete Section 2 comparisons.js

--- section2/exercises/exercises/comparisons.js
+++ section2/exercises/exercises/comparisons.js
@@ -5,7 +5,7 @@ to achieve the desired result.
 One example is already completed. Your task is to complete
 any remaining prompt.
 
-Make sure to run the file with node in your command line. Look back 
+Make sure to run the file with node in your command line. Look back
 at the directions from Section 1 if you need a refresher on how to do that.
 */
 
@@ -19,13 +19,15 @@ var stringTeachers = "4";
 var numberDogs = 0;
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents); 
+console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 // this should log: true
 
 /*
@@ -35,6 +37,7 @@ perform this evaluation
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 // this should log: false
 
 /*
@@ -48,18 +51,23 @@ you develop good habits that follow best practice, from now on, use the strict c
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log("Is numberStudents less than or equal to 21", numberStudents <= 21);
 // this should log: true
 
 
@@ -67,27 +75,31 @@ you develop good habits that follow best practice, from now on, use the strict c
 // PART 2: Articulating what you are doing
 //-------------------
 
-// For the following prompts, you will be given a line of code and your task is to type out a Comment, 
-// in English, explaining what that line of code is doing, including what the comparison will evaluate to. 
-// Be as technically precise as possible, but don't just copy and paste a definition from the readings. 
+// For the following prompts, you will be given a line of code and your task is to type out a Comment,
+// in English, explaining what that line of code is doing, including what the comparison will evaluate to.
+// Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
 //YOU DO: Explain.
+//Should log true as 4 is less than 9
 
 var books = 3;
-console.logs(4 < books);
+console.log(4 < books);
 // YOU DO: Explain.
+//Should log false as 4 is less than 3, which is what the variable books equates to
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+//Should log true as friends equates to 6 and siblings equates to 2, and 6 is greater than 2
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
+//Should log true as attendees equates to 9 and meals equates to 8 and 9 does not equal 8
 
 
 //-------------------
@@ -110,22 +122,24 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log("Does the dog love to play and love treats?", lovesToPlay && lovesTreats);
 
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log("Does the dog love to play and love the dog park?", lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log("Does the dog love to play or love the dog park?", lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log("Does the dog love to play and is it a puppy?", lovesToPlay && age);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: since age is a non-boolean operator the program will try to coerce lovesToPlay to be false. Since that cannot happen it will display 1 (what age equates to)
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-  // as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+  // as you would expect?
