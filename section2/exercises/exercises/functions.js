8d0911ffa6e4c54cf6c004955ab0b28773b36916
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-10-15 15:08:35 -0500

    Update functions.js

--- section2/exercises/exercises/functions.js
+++ section2/exercises/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,12 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -35,18 +41,37 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
+function greetMe(name) {
+  console.log(name);
+};
 
+greetMe("Connor");
+greetMe("Krista");
+greetMe("Allie");
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
+function numbers(num1, num2){
+  console.log(num1 + num2);
+};
 
+numbers(8,2);
+numbers(3,7);
+numbers(4,6);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function jump(animal1, animal2){
+  console.log("The quick brown " + animal1 + " jumps over the lazy " + animal2 + ".");
+};
 
+jump('porcupine', 'walrus')
+jump('salmon', 'bear')
+jump('manitee', 'shark')
+jump('fox', 'dog')
 
 //-------------------
 // PART 3: Naming is Hard
@@ -60,6 +85,7 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
-
+The primary action in the logged string is jump, so I figured that would be a good way to describe the function.
+The parameters are named animal1 and animal2 to show the function is best run with two animals as arguments over anything else.
 
 */
