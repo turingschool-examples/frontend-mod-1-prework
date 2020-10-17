9352e96fdc8bab14332ab7533cb6541c35c34bf3
Nikki <piknik@me.com>
2020-10-17 10:04:32 -0600

    oops forgot datatypes

--- section1/exercises/dataTypes.js
+++ section1/exercises/dataTypes.js
@@ -21,10 +21,10 @@ console.log("Alan Turing");
 console.log('Welcome to Turing!');
 
 // YOU DO: Write code below to log `99 bottles of pop on the wall...`:
-
+console.log('99 bottles of pop on the wall...');
 
 // YOU DO: Write code below to log one line from your favorite song or movie:
-
+console.log('Why don\'t you take the chance, I\'ve got the moves I\'d like to prove, I wanna slow dance with youuuu.');
 
 
 //-------------------
@@ -32,19 +32,19 @@ console.log('Welcome to Turing!');
 //-------------------
 
 // EXAMPLE: log the result of the sum of 2 and 2 to the console:
-console.log(2 + 2);
+console.log("addition: ", 2 + 2);
 
 // EXAMPLE: log the result of 7 subtracted from 83 console:
-console.log(83 - 7);
+console.log("subtraction: ", 83 - 7);
 
 // YOU DO: log the result of 6 multiplied by 53 to the console:
-
+console.log("multiplication: ", 6 * 53);
 
 // YOU DO: log the result of 20 divided by 4 to console:
-
+console.log("division: ", 20 / 4);
 
 // YOU DO: log the result of the modulo of 10 into 54:
-
+console.log("modulo: ", 54 % 10);
 
 
 //-------------------
@@ -52,16 +52,19 @@ console.log(83 - 7);
 //-------------------
 
 // EXAMPLE: log to the console the result of 1 is equal to 2:
-console.log(1 === 2);
+console.log("strict equals: ", 1 === 2);
 
 // EXAMPLE: log to the console the result of 7 is greater than 2:
-console.log(7 > 2);
+console.log("greater than: ", 7 > 2);
 
 // YOU DO: log to the console the result of "hello" is equal to "Hello":
+console.log("strict equals: ", 'hello' === 'Hello'); // doesn't specify, so I used strict
 
 // YOU DO: log to the console the result of 3 is not equal to 4:
+console.log("not equal: ", 3 != 4);
 
 // YOU DO: log to the console the result of 4 is less than or equal to 5:
+console.log("less than or equal: ", 4 <= 5);
 
 
 
