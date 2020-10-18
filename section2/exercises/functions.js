d782fc022db82753200aadd59183a69da8f06b70
Nikki <piknik@me.com>
2020-10-18 11:01:55 -0600

    add function instructional video

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -1,73 +1,73 @@
-// /*
-// In the exercises below, write your own code where indicated
-// to achieve the desired result.
-//
-// One example is already completed. Your task is to complete
-// any remaining prompt.
-//
-// Make sure to run the file with node in your command line.
-// */
-//
-// //-------------------
-// // PART 1: Function Syntax
-// //-------------------
-//
-// // EXAMPLE: Write a function below that, when called will log "Severus Snape" to the console
-// function printName() {
-//   console.log("Severus Snape");
-// };
-//
-// printName();
-// printName();
-// printName();
-// printName();
-// // Since the function was called 4 times, "Severus Snape" will log 4 times.
-//
-//
-// // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
-// // Then, call the function 2 times.
-// function sayHello() {
-//   console.log("Oh, Hello!");
-// }
-//
-// sayHello();
-// sayHello();
-//
-// //-------------------
-// // PART 2: Arguments and Parameters
-// //-------------------
-//
-// // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
-// // The function should print out the value of the String that was passed in.
-// // Then, call the function 3 times, each time, passing it a different name.
-// function greetMe(name) {
-//   console.log("oh hai, " + name + "!");
-// }
-//
-// greetMe("Marceline");
-// greetMe("Bubblegum");
-// greetMe("Fionna");
-//
-//
-// // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
-// // Then, call that function 3 times, each time, passing in 2 different Numbers.
-// function sumItUp(num1, num2) {
-//   console.log(num1 + num2);
-// }
-//
-// sumItUp(21, 21);
-// sumItUp(42, 42);
-// sumItUp(55, 100);
-//
-// // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
-// // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
-// // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-//
-// function concatThis(str1, str2) {
-//   console.log(`${str1} and ${str2} are BFFs.`)
-// }
-//
-// concatThis("Nikki", "Susan")
+/*
+In the exercises below, write your own code where indicated
+to achieve the desired result.
+
+One example is already completed. Your task is to complete
+any remaining prompt.
+
+Make sure to run the file with node in your command line.
+*/
+
+//-------------------
+// PART 1: Function Syntax
+//-------------------
+
+// EXAMPLE: Write a function below that, when called will log "Severus Snape" to the console
+function printName() {
+  console.log("Severus Snape");
+};
+
+printName();
+printName();
+printName();
+printName();
+// Since the function was called 4 times, "Severus Snape" will log 4 times.
+
+
+// YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
+// Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+}
+
+sayHello();
+sayHello();
+
+//-------------------
+// PART 2: Arguments and Parameters
+//-------------------
+
+// YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
+// The function should print out the value of the String that was passed in.
+// Then, call the function 3 times, each time, passing it a different name.
+function greetMe(name) {
+  console.log("oh hai, " + name + "!");
+}
+
+greetMe("Marceline");
+greetMe("Bubblegum");
+greetMe("Fionna");
+
+
+// YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
+// Then, call that function 3 times, each time, passing in 2 different Numbers.
+function sumItUp(num1, num2) {
+  console.log(num1 + num2);
+}
+
+sumItUp(21, 21);
+sumItUp(42, 42);
+sumItUp(55, 100);
+
+// YOU DO🎈: Write a function that takes in two strings and prints a concatenation
+// of those two strings, for example the arguments could be ("Oscar", "Ruck") and
+// the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+
+function concatThis(str1, str2) {
+  console.log(`${str1} and ${str2} are BFFs.`)
+}
+
+concatThis("Nikki", "Susan")
 
 
 //-------------------
@@ -91,12 +91,16 @@ arguments based on what they are, also in hopes of conveying their intended use.
 // PART 4: Video teaching about Functions
 //-------------------
 
-// write a function
-console.log("hello")
+// What is a function? And how are they named?
+// addition addTheseNumbers
 
+// Write a function
+function square(number) {
+    return number * number;
+}
 
-// call the function
-
+// Call the function
+console.log(square(4));
 
 
 
