c76c801b0c59fcb50e2890c3e7bf51f89ec614b3
Nikki <piknik@me.com>
2020-10-22 13:37:36 -0600

    added the video of instruction

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -8,13 +8,53 @@ Create several instances of your Tweet and log them to the console. Make sure th
 tweet object instances _behave_ as expected.
 */
 
+// class Tweet {
+//     constructor(author, content, timeStamp, numberOfLikes, comments) {
+//         this.author = author;
+//         this.content = content;
+//         this.timeStamp = timeStamp;
+//         this.numberOfLikes = numberOfLikes;
+//         this.comments = comments;
+//     }
+//
+//     incrementLikes() {
+//         this.numberOfLikes++;
+//     }
+//
+//     addComments(newComment) {
+//         this.comments.push(newComment)
+//     }
+// }
+//
+// var tweet1 = new Tweet('nikki', 'dear twitter: what is the weather like today?', new Date(), 4, []);
+// console.log(tweet1);
+// tweet1.incrementLikes();
+// console.log(tweet1);
+// tweet1.addComments('hey it\'s pretty nice here in florida.')
+// console.log(tweet1);
+// tweet1.addComments('it\'s gross in michigan.')
+// console.log(tweet1);
+//
+// //
+// var tweet2 = new Tweet('my cat', 'meow', new Date(), 500, ['amen, sister!', 'meow right back at you!', 'rawr', 'purr']);
+// console.log(tweet2);
+// tweet2.incrementLikes();
+// tweet2.incrementLikes();
+// tweet2.incrementLikes();
+// tweet2.incrementLikes();
+// tweet2.incrementLikes();
+// tweet2.incrementLikes();
+// tweet2.addComments('funny hoomans don\'t know my hairballs!')
+// console.log(tweet2);
+
+
 class Tweet {
     constructor(author, content, timeStamp, numberOfLikes, comments) {
-        this.author = author;
-        this.content = content;
-        this.timeStamp = timeStamp;
-        this.numberOfLikes = numberOfLikes;
-        this.comments = comments;
+        this.author = author
+        this.content = content
+        this.timeStamp = timeStamp
+        this.numberOfLikes = numberOfLikes
+        this.comments = comments
     }
 
     incrementLikes() {
@@ -26,23 +66,34 @@ class Tweet {
     }
 }
 
-var tweet1 = new Tweet('nikki', 'dear twitter: what is the weather like today?', new Date(), 4, []);
-console.log(tweet1);
-tweet1.incrementLikes();
-console.log(tweet1);
-tweet1.addComments('hey it\'s pretty nice here in florida.')
-console.log(tweet1);
-tweet1.addComments('it\'s gross in michigan.')
+var tweet1 = new Tweet('nikki', 'I like to tweet.', new Date(), 5, []);
 console.log(tweet1);
 
-//
-var tweet2 = new Tweet('my cat', 'meow', new Date(), 500, ['amen, sister!', 'meow right back at you!', 'rawr', 'purr']);
-console.log(tweet2);
-tweet2.incrementLikes();
-tweet2.incrementLikes();
-tweet2.incrementLikes();
-tweet2.incrementLikes();
-tweet2.incrementLikes();
-tweet2.incrementLikes();
-tweet2.addComments('funny hoomans don\'t know my hairballs!')
-console.log(tweet2);
+tweet1.incrementLikes();
+tweet1.incrementLikes();
+console.log(tweet1);
+
+tweet1.addComments('hey I like to tweet, too!!');
+console.log(tweet1);
+
+tweet1.addComments('hey I don\'t like to tweet at all :(');
+console.log(tweet1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
