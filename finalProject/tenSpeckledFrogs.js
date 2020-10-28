ff9e14b214488dd8f25ca7713022f4dacb9a095a
Nikki <piknik@me.com>
2020-10-28 16:51:11 -0600

    last part of the frogs

--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -1,19 +1,167 @@
-function frogCountDown() {
-    // for loop to go through 10 to 1 count down
-    // consider special cases of 3, 2, 1
+// function frogCountDown() {
+//     // for loop to go through 10 to 1 count down
+//     // consider special cases of 3, 2, 1
+//
+//     for (let i = 10; i > 0; i--) {
+//         if (i > 2) {
+//             console.log(`${i} speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it's nice and cool, then there were ${i-1} speckled frogs.`);
+//         } else if (i === 2) {
+//             console.log(`${i} speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it's nice and cool, then there was ${i-1} speckled frog.`);
+//         } else if (i === 1) {
+//             console.log(`${i} speckled frog sat on a log eating some most delicious bugs.`);
+//             console.log(`It jumped in the pool where it's nice and cool, then there were no more speckled frogs!`);
+//         }
+//         console.log('');
+//     }
+// }
+//
+// frogCountDown();
 
-    for (let i = 10; i > 0; i--) {
-        console.log(`${i} speckled frogs sat on a log eating some most delicious bugs.`);
+// Extension 1
+// function frogCountDown(start, stop) {
+//     // for loop to go through 10 to 1 count down
+//     // consider special cases of 3, 2, 1
+//
+//     for (let i = start; i >= stop; i--) {
+//         if (i > 2) {
+//             console.log(`${i} speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it's nice and cool, then there were ${i-1} speckled frogs.`);
+//         } else if (i === 2) {
+//             console.log(`${i} speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it's nice and cool, then there was ${i-1} speckled frog.`);
+//         } else if (i === 1) {
+//             console.log(`${i} speckled frog sat on a log eating some most delicious bugs.`);
+//             console.log(`It jumped in the pool where it's nice and cool, then there were no more speckled frogs!`);
+//         }
+//         console.log('');
+//     }
+// }
+//
+// frogCountDown(7, 1);
 
-        if (i > 2) {
-            console.log(`One jumped in the pool where it's nice and cool, then there were ${i-1} speckled frogs.`);
-        } else if (i === 2) {
-            console.log(`One jumped in the pool where it's nice and cool, then there was ${i-1} speckled frog.`);
-        } else if (i === 1) {
-            console.log(`One jumped in the pool where it's nice and cool, then there were no more speckled frogs!`);
-        }
-        console.log('');
+// Extension 2 -- I was super confused about the instructions. I'm not sure how to do it without setting up an
+// object that holds the number and the word, so I am trying to set up just an array and use the index position
+// of the words in relationship to the number of the iteration in the loop. Full disclosure: I copy/pasted the
+// title case function
+
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+
+function frogCountDown(start, stop) {
+    var wordArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+    for (let i = start; i >= stop; i--) {
+    var numWord = wordArray[i-1]; // this is the index of the loop number's word in the array
+    var numWordLessOne = wordArray[i-2]; // this is the index of one less than that
+
+    if (i > 2) {
+        console.log(`${titleCase(numWord)} speckled frogs sat on a log eating some most delicious bugs.`);
+        console.log(`One jumped in the pool where it's nice and cool, then there were ${numWordLessOne} speckled frogs.`);
+    } else if (i === 2) {
+        console.log(`${titleCase(numWord)} speckled frogs sat on a log eating some most delicious bugs.`);
+        console.log(`One jumped in the pool where it's nice and cool, then there was ${numWordLessOne} speckled frog.`);
+    } else if (i === 1) {
+        console.log(`${titleCase(numWord)} speckled frog sat on a log eating some most delicious bugs.`);
+        console.log(`It jumped in the pool where it's nice and cool, then there were no more speckled frogs!`);
+    } else {
+        console.log(`${titleCase(numWord)} speckled frog sat on a log eating some most delicious bugs.`);
+        console.log(`One jumped in the pool where it's nice and cool, then there was ${numWordLessOne} speckled frog.`);
+    }
+    console.log('');
     }
 }
 
-frogCountDown();
+frogCountDown(10, 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// switch isn't terribly efficient for this use case, but it can be done
+// function frogCountDown(num) {
+//
+//     switch (num) {
+//         case 10:
+//             console.log(`Ten speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were nine speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 9:
+//             console.log(`Nine speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were eight speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 8:
+//             console.log(`Eight speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were seven speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 7:
+//             console.log(`Seven speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were six speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 6:
+//             console.log(`Six speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were five speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 5:
+//             console.log(`Five speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were four speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 4:
+//             console.log(`Four speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were three speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 3:
+//             console.log(`Three speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there were two speckled frogs.`)
+//             console.log('');
+//             break;
+//         case 2:
+//             console.log(`Two speckled frogs sat on a log eating some most delicious bugs.`);
+//             console.log(`One jumped in the pool where it\'s nice and cool, then there was one speckled frog.`)
+//             console.log('');
+//             break;
+//         case 1:
+//             console.log(`One speckled frog sat on a log eating some most delicious bugs.`);
+//             console.log(`It jumped in the pool where it\'s nice and cool, then there were no more speckled frogs!`)
+//             console.log('');
+//             break;
+//         default:
+//             console.log('no frogs left')
+//     }
+// }
+//
+// frogCountDown(4);
+// frogCountDown(3);
+// frogCountDown(2);
+// frogCountDown(1);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -34,19 +34,8 @@ tweet object instances _behave_ as expected.
 // console.log(tweet1);
 // tweet1.addComments('it\'s gross in michigan.')
 // console.log(tweet1);
-//
-// //
-// var tweet2 = new Tweet('my cat', 'meow', new Date(), 500, ['amen, sister!', 'meow right back at you!', 'rawr', 'purr']);
-// console.log(tweet2);
-// tweet2.incrementLikes();
-// tweet2.incrementLikes();
-// tweet2.incrementLikes();
-// tweet2.incrementLikes();
-// tweet2.incrementLikes();
-// tweet2.incrementLikes();
-// tweet2.addComments('funny hoomans don\'t know my hairballs!')
-// console.log(tweet2);
 
+//
 
 class Tweet {
     constructor(author, content, timeStamp, numberOfLikes, comments) {
@@ -79,21 +68,50 @@ console.log(tweet1);
 tweet1.addComments('hey I don\'t like to tweet at all :(');
 console.log(tweet1);
 
+var tweet2 = new Tweet('my cat', 'meow', new Date(), 500, ['amen, sister!', 'meow right back at you!', 'rawr', 'purr']);
+console.log(tweet2);
+tweet2.incrementLikes();
+tweet2.incrementLikes();
+tweet2.incrementLikes();
+tweet2.incrementLikes();
+tweet2.incrementLikes();
+tweet2.incrementLikes();
+tweet2.addComments('funny hoomans don\'t know my hairballs!')
+console.log(tweet2);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// class Child {
+//     constructor(name, hairType, isPlaying, snacksAvailable, favoriteGame) {
+//         this.name = name;
+//         this.hairType = hairType;
+//         this.isPlaying = isPlaying
+//         this.snacksAvailable = snacksAvailable;
+//         this.favoriteGame = favoriteGame;
+//     }
+//
+//     scheduleHaircut() {
+//         if (this.name === 'Bert' || this.hairType === 'curly') {
+//             return 'Please schedule an appointment to cut your hair!'
+//         }
+//     }
+//
+//     playsGames(mySnacks, currentGames) {
+//         if (this.snacksAvailable === mySnacks && this.favoriteGame === currentGames) {
+//             this.isPlaying = true;
+//             console.log('Play on!');
+//         } else {
+//             console.log('no games for you!!')
+//         }
+//     }
+// }
+//
+// var bert = new Child('Bert', 'curly', false, 'chips', 'Smash Bros')
+// var oscar = new Child('Oscar', 'semi-curly', false, 'apples', 'Minecraft')
+// var vera = new Child('Vera', 'straight', false, 'granola bars', 'Roblox')
+//
+// console.log(bert);
+// console.log(oscar);
+// console.log(vera);
+//
+// console.log(bert.scheduleHaircut());
+// console.log(vera.playsGames('granola bars', 'Roblox'));
