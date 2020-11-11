35e65dc8a946d1114c7a389f7b6030e221965eeb
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-11-10 18:57:43 -0700

    Update fizzBuzz final project

--- finalProject/fizzBuzz.js
+++ finalProject/fizzBuzz.js
@@ -1,12 +1,13 @@
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 == 0) {
-    console.log("Fizz");
+  if (i % 3 == 0 && i % 5 == 0 ) {
+    console.log("FizzBuzz");
   }
   else if (i % 5 == 0) {
     console.log("Buzz");
   }
-  else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+  else if (i % 3 == 0)
+     {
+    console.log("Fizz");
   }
   else {
     console.log(i);
