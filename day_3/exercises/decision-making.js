d6e8f3a40d2e6e4e6cabe6ed01a94066c28d22b3
Patrick Findley <patfindley@gmail.com>
2020-12-22 19:32:25 -0500

    Add day 3 work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -10,7 +10,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 3;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -31,7 +31,7 @@ if (bearChoice === 1) {
   console.log("You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!");
 } else if (bearChoice === 2) {
   console.log("You tell the bear the " + bearClothing + " is too small and it starts to cry!");
-} else if (bearClothing === 3) {
+} else if (bearChoice === 3) { // changed var bearClothing to bearChoice
   console.log("You run as fast as you can into the next room. It's full of snakes!");
 } else {
   console.log("You stay with the bear and become it's best friend!");
@@ -40,16 +40,36 @@ if (bearChoice === 1) {
 /*
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
+  line 17 evaluates if the variable doorChoice is strictly equal to the integer 1
+  the "if" statement declares if the statement IS true, the code on line 18 will be used.
+  line 18 reassigns the variable bearClothing the value of "hat"
+  lines 19 contains the statement "else" which declares that if the statement on line 17 evaluates to false, then the code on line 20 will be used
+  line 20 assigns the variable bearClothing the value of "scarf"
+  line 21's curly bracket ends the evaluation
 
 2. What variable has a new value assigned to it after the first if statement executes?
+  The variable bearClothing will be reassigned "hat" from no assigned value
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+  the bearClothing variable will be assigned the value of "scarf" if anything besides the integer 1 is used
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+  line 31's console.log will output the string "You off the bear your " followed by the value of the variable bearClothing, followed by the string " and the bear shows you a secret passage out!"
+  line 32 states that if line 30 is false BUT 32 is TRUE in the value of bearChoice being strictly equal to the integer 2, to run line 33
+  line 33's console.log will output the string "you tell the bear the " following by the value of bearClothing followed by " is too small and it starts to cry!"
+  line 34 states that if line 30 is false, as well as line 32, but the value of bearClothing IS strictly equal to 3, then run the code on line 35
+  line 35's console.log will output the string "you run as fast as you can into the next room. It's full of snakes!"
+  line 36 states if lines 30, 32, and 34's evaluations all come up false, then use the code on line 37
+  line 37's console.log will output the string "You stay with the bear and become it's best friend!"
+  line 38 ends the evaluations
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+  all of the else if's conditions return false, and the else code runs, leaving you in constant fear of befriending a bear.
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+  doorChoice value of 1 will evaluate line 17 to true, reassiging the var bearClothing the value of "hat"
+  bearChoice value being 2 will check false with line 30's condition, but true with 32's condition and output the string "You tell the bear the hat is too small and it starts to cry"
 
 7. What is your favorite ending?
+  My favorite ending is by far the hidden snakes ending.
 */
--- day_3/questions.md
+++ day_3/questions.md
@@ -1,13 +1,48 @@
 ## Day 3 Questions
 
 1. What is a conditional statement? Give one example of a daily life conditional. Give one example of where a conditional is probably used in a web application you use.
+  * A ``conditional statement`` allows code to make decisions about what to do next.
+  * a daily life condition may be
+  ```javascript
+
+  var isBodyVibrating = false;
+
+  if (isBodyVibrating == false) {
+    console.log("Have another cup of coffee!");
+  } else {
+    console.log("Put the coffee mug down and step away slowly!");
+  }
+
+  ```
+  * an example of a conditional statement used in a web app i use might be e-mail auto refresh
+  ```javascript
+
+var minutesPassed = 5
+if (minutesPassed < 5) {
+(refresh email) /* code thats currently beyond my current comprehension */
+}
+```
 
 2. Why might you want to use an `if` statement?
+  * you would want to use in `if` statement when you have code that does not need to be running all of the time, or code that gives multiple output options based on user input or data recieved.
 
 3. What is the JavaScript syntax for an `if` statement?
+ * ```if (condition === otherCondition) {
+   (code to be ran);
+ }
+```
+  * ``===`` the ``strictly equal to`` is just a placeholder in that syntax and is just one of many ``comparison operators``.
 
 4. How do you add multiple conditions to an `if` statement?
+  * the ``else if`` statement allows you to add multiple conditons to an `if` statement.
 
 5. What is the JavaScript syntax for an `if/else` statement?
+  * ```if (condition >= otherCondition) {
+    console.log("Your condition must be greater than or equal to otherCondition!");
+  }  else {
+      console.log("Your condition seems to be less than otherCondition.");
+    }
+  ```
 
 6. Other than an `if` statement, can you think of any other ways we might want to use a conditional statement?
+  * the `for` statement can execute a block of statements if a condition returns true. 
