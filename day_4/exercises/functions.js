42fa4a712bf595a4724795edeaa055aaab7207a1
Ryan Miller <ryandmiller3521@gmail.com>
2020-12-27 13:33:49 -0700

    Add Day 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,34 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
+function printNames() {
   console.log("Severus Snape");
-};
+}
 
-printName();
+printNames();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName(name) { // hint.. add a parameter on this line :)
+  console.log(name);
 };
 
 printName("Albus Dumbledore");
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+function numberSum(num1, num2){
+  console.log(num1 + num2);
+}
+
+numberSum(22, 24);
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-// YOUR CODE HERE
+function combineString(string1, string2){
+  console.log(string1 + " and " + string2 + " Are going to have a great day!")
+}
+
+combineString("BoB", "Ross");
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,15 +1,22 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a function?
-
-2. What is a parameter? 
-
+A function allows you to call a block of code that you might use multiple times without having to rewrite that code block
+It also allows for information to be passed to and from it.
+2. What is a parameter?
+A parameter is a variable that is passed into a function.
 3. What is a return value?
+A return vaule is a variable that is passed back from the function to where it was called from.
 
 4. In the space below, declare a function named `hello` that will print to the console, `"Sam I am"`.
-
+function hello(){
+  console.log("Sam I am");
+}
 5. Declare a function name `helloSomeone` that takes an argument of `name` and logs `name + " I am"`.
-
+function helloSomeone(name){
+  console.log(name + " I am");
+}
 6. How would you _call_ or _execute_ the function that you created above?
-
+helloSomeone("Bob Ross");
 7. What questions do you still have about functions in JavaScript?
+I currently do not have an question but im sure as we get more into them I will have questions.
