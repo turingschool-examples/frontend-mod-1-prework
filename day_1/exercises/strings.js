e8c7cbe36a083edc7e759a89f131ffff6d475b90
Robert DeRouin <robertjosephderouin@gmail.com>
2021-01-08 11:11:23 -0700

    Add day 1

--- day_1/exercises/strings.js
+++ day_1/exercises/strings.js
@@ -8,10 +8,20 @@ file from your terminal with the command `node day_1/exercises/strings.js`
 console.log("Alan Turing");
 
 // Write code below to log `Welcome to Turing!` in the console:
-console.log(/*YOUR CODE HERE*/);
+console.log("Robert DeRouin");
 
 // Write code below to log `99 bottles of pop on the wall...`:
-// YOUR CODE HERE
+console.log("99 bottles of pop on the wall...")
 
 //Write code below to log one line from your favorite song or movie:
-// YOUR CODE HERE
+console.log("Open the pod bay doors HAL!")
+
+/*robertsmacbookpro@Roberts-MacBook-Pro exercises % ls
+booleans.js		numbers.js		variables.js
+concatenation.js	strings.js
+robertsmacbookpro@Roberts-MacBook-Pro exercises % node strings.js
+Alan Turing
+Robert DeRouin
+99 bottles of pop on the wall...
+Open the pod bay doors HAL!
+robertsmacbookpro@Roberts-MacBook-Pro exercises % */
