15edafbcbe4111e52f6ddf4f98d69e551a5f5e7f
Joshua Horner <lesliewbusiness@gmail.com>
2021-04-19 12:57:32 -0600

    Add Section 1

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -22,8 +22,8 @@ var creatures = "unicorns";
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
-
+// Your answer and explanation here:
+// The console will log a string as it combines words and variables.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,6 +31,7 @@ console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log(`The ${speedy} jumped over the ${slowPoke}.`)
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -40,16 +41,22 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}!`)
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
+var moveAndGroove = "bass";
+var trebleMaker = "guitar";
+var smashAndCrash = "drums";
 
+console.log(`The ${trebleMaker} is cool and all, but the ${moveAndGroove} and ${smashAndCrash} are what really bring the funk!`)
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
+// You know it!
--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,33 +23,52 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var dobbyWarning = 'Harry Potter must not return to Hogwarts!'
+console.log(dobbyWarning);
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
+var firstName = "Josh";
+var isHungry = true;
+var numberOfPets = 0;
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
 
+// My name would need to be a string because it contains only letters.
+// The variable "isHungry" is a boolean, so it would have to be true or false.
+// The variable "numberOfPets" would naturally result in a number, so I formatted it as such.
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
+firstName = "Jorsh"; // change variable firstName
+isHungry = false; // change variable isHungry
+numberOfPets = 1; // change variable numberOfPets
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
 
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
+var snackCount = (healthySnacks + junkFoodSnacks);
+console.log(snackCount);
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
+// Yes!
