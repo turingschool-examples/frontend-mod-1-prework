b5af9e5c317d0ee2062bbebfe26c6aed1f899b28
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 11:44:53 -0600

    Answerloops questions

--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -13,32 +13,50 @@ Make sure to run the file with node in your command line.
 //-------------------
 
 // EXAMPLE: Write code that logs to the console "I love hedgehogs!" 9 times:
+
 for (var i = 0; i < 9; i++) {
   console.log("I love hedgehogs!");
 }
 
 // EXAMPLE: Write code that logs to the console the value of `i`, 3 times:
+
 for (var i = 0; i < 3; i++) {
   console.log(i);
 }
 
 // What is logged to the console for this second example? Why?
 // EXPLAIN:
-
+//
+// 0 , 1 , 2 on seperate lines
+// The intialization starts the variable (i) at 0.
+// The condition states that the interpreter runs as long as "i" < 3.
+// (i) is updated with the `i++` instruction, increasing by one each time.
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
+
+ for (var i = 0; i < 7; i++) {
+   console.log(2 + 2);
+ }
 
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
+
+ for (var i = 0; i < 10; i++) {
+   console.log("She sells seashells down by the seashore");
+ }
 
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
+
+ for (var i = 0; i <3; i++) {
+   console.log("This is number:"+ i);
+ }
 
 // YOU DO: Using a for loop, how could you get an output that looks like this:
 
@@ -53,6 +71,11 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+for (var i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your pre-work group on Slack!
