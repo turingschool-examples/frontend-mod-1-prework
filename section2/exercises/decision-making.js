87aae8c9f4490d6628cb8ad604ca8ad97bd65446
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 10:03:27 -0600

    Answer decision-making questions

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -39,15 +39,36 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
+    We have a condtional if/else statement that determines what the bear is wearing.
+
+    If the value of the variable `doorChoice` is strictly equal to 1,
+    the variable `bearClothing` will be assigned to "hat." This results in a `true`
+    statement and the code will proceed to the next block.
+
+    If the value of `doorChoice` is not strictly 1, then the `else` condition will be executed.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+    `bearClothing`
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+    `scarf`
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+
+    We are using the `else if` statement. In this scenario, the if/else if/else structure can only execute one section of instructions run.
+    Javascript will read unitl it registers a `true` statement, and not bother with the rest of the code block.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+    Hopefully you are not Indiana Jones, as you'll be dealing with a room full of snakes.
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+    The bear will be told its hat is too small and it will start to cry.
+
 7. What is your favorite ending?
+
+    Certainly the secret door... cause I'm Indiana Jones! :)
 */
