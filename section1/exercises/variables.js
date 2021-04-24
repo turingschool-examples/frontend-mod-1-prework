35c904e638c6a032d959bef824afeafee0303290
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 09:50:30 -0600

    Answer variables quesitons

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -12,11 +12,13 @@ file from your terminal with the command `node section1/exercises/variables.js`
 
 // EXAMPLE: Write code below to assign Harry Potter's name to a variable and
 // log that variable to the console:
+
 var name = "Harry Potter";
 console.log(name);
 
 // EXAMPLE: Write code below to add 2 to the variable `students` and
 // log the result:
+
 var enrolledStudents = 22 + 2;
 console.log(enrolledStudents);
 
@@ -24,32 +26,55 @@ console.log(enrolledStudents);
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
 
+var banish = "Harry Potter must not return to Hogwarts!";
+console.log(banish);
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
-  // Store the appropriate data types in each.
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
+// Store the appropriate data types in each.
 // log all three variables to the console.
+
+var firstName = "Justin";
+console.log(firstName);
+var isHungry = true;
+console.log(isHungry);
+var numberOfPets = 1;
+console.log(numberOfPets);
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
-  // Explain.
+// How did you decide to use the data type you did for each of the three variables above?
+// Explain.
+
+/*- I used a _string_ for the `firstName` becuase of the characters used for the answer have letters.
+  - I used a _boolean type_ for `isHungry` becuase we are asking a yes or no question
+  - I used a _number_ for `numberOfPets` becasue the answer is a number.*/
 
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
-  // values (but same data type).
+// Re-assign the values to the three variables from the previous challenge to different
+// values (but same data type).
 // log all three variables to the console.
+
+firstName = "James";
+console.log(firstName);
+isHungry = false;
+console.log(isHungry);
+numberOfPets = 0;
+console.log(numberOfPets);
+
 
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
+
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
+console.log(healthySnacks + junkFoodSnacks);
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
