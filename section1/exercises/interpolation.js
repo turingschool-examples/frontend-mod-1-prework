34686623b72e8c5ef86e9d5d795b1f6d946e4d67
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 09:51:50 -0600

    Answer interpolation questions

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -10,27 +10,32 @@ file from your terminal with the command `node section1/exercises/interpolation.
 */
 
 // EXAMPLE: Write code that combines the variables below into another string:
+
 var name = "Ron";
 var team = "Chudley Cannons";
 
 console.log(`The ${team} are ${name}'s favorite Quidditch team.`);
 
 // YOU DO: Read the next few lines of code...
+
 var numberOfCreatures = 7;
 var creatures = "unicorns";
 
-console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
+console.log(`My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
 
+// This will log the string "My zoo has 7 unicorns!" This is the product of type coercion.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "The quick red fox jumped over the lazy brown dog":
+
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log(`The ${speedy} jumped over the ${slowPoke}`);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -40,16 +45,22 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}!`)
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
 
+var difficulty = "default";
+var videoGame = "Nioh 2";
+var gamingExperience = "rage inducing";
+
+console.log(`The ${difficulty} difficulty setting in ${videoGame} is ${gamingExperience}!`);
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
