189956e03028f590c9dc4f4daea07e36af0b0419
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 12:16:58 -0600

    Answer classProperties questions

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,42 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
+class ExampleDog {
+
+};
+
+var doggoOne = new ExampleDog();
+console.log(doggoOne);
+
+var doggoTwo = new ExampleDog();
+console.log(doggoTwo);
+
 
 // Prompt 2: Snack
 
+class ExampleSnack {
+
+}
+
+var fruit = new ExampleSnack()
+console.log(fruit);
+
+var chips = new ExampleSnack()
+console.log(chips);
+
+
 // Prompt 3: Shirt
+
+class ExampleShirt {
+
+}
+
+var dressShirt = new ExampleShirt()
+console.log(dressShirt);
+
+var tShirt = new ExampleShirt()
+console.log(tShirt);
+
 
 //-------------------
 // Part 2: Properties
@@ -34,9 +66,46 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class DogOne {
+  constructor(name, breed, age, weight, isGoodBoy) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.weight = weight;
+    this.isGoodBoy = isGoodBoy;
+  }
+};
+
+var newDog = new DogOne();
+console.log(newDog);
+
+
+
 // Prompt 2: Snack
 
+class SnackOne {
+  constructor(brand, type, calories) {
+    this.brand = brand;
+    this.type = type;
+    this.calories = calories;
+  }
+};
+
+var apple = new SnackOne();
+console.log(apple);
+
 // Prompt 3: Shirt
+
+class ShirtOne {
+  constructor(occasion, type, size) {
+    this.occasion = "occasion";
+    this.size = "size";
+    this.price = "price";
+  }
+};
+
+var tankTop = new ShirtOne();
+console.log(tankTop);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +121,41 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+  constructor(name, breed, age, weight, isGoodBoy) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.weight = weight;
+    this.isGoodBoy = isGoodBoy;
+  }
+};
+
+var newDog = new Dog("Pilgrim", "Siberian Husky", 7, "50lbs", true);
+console.log(newDog);
+
+
 // Prompt 2: Snack
 
+class Snack {
+  constructor(brand, type, calories,) {
+    this.brand = brand;
+    this.type = type;
+    this.calories = calories;
+  }
+};
+
+var apple = new Snack("local farm", "fruit", 90);
+console.log(apple);
 // Prompt 3: Shirt
+
+class Shirt {
+  constructor(occasion, size, price) {
+    this.occasion = occasion;
+    this.size = size;
+    this.price = price;
+  }
+};
+
+var tankTop = new Shirt("casual", "medium", "$25" );
+console.log(tankTop);
