4db89e42a4c8f5ae55e4d03220bf400c5ce6f0a5
Justin Anthony <justincanthony@yahoo.com>
2021-04-24 10:04:42 -0600

    Answer if-statements questions

--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -12,8 +12,8 @@ Make sure to run the file with node in your command line.
 // EXAMPLE:Manipulate the variable 'weather' to see if you can print something other
   // than 'coat'.
 
-var weather = 'snowy';
-// var weather = 'icy';
+// var weather = 'snowy';
+var weather = 'icy';
 // var weather = 'rainy';
 // var weather = 'sunny';
 // While working through this example, I would toggle which variable declaration is
@@ -31,10 +31,10 @@ if (weather == 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
@@ -43,7 +43,13 @@ Log to the console the appropriate age range
 
 var dogAge = 3;
 // Write your conditional here
-
+if (dogAge <= 2) {
+  console.log('puppy');
+} else if (dogAge >= 10) {
+  console.log('elderly');
+} else{
+  console.log('adult');
+}
 
 /*
 YOU DO:
@@ -62,12 +68,13 @@ When numQuarters = 2, program should log "I have enough money for a gumball"
 When numQuarters = 3, program should log "I have enough money for a gumball"
 */
 
-var numQuarters = 0;
+var numQuarters = 100;
 
-// Write your conditional here
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if (numQuarters < 2) {
+  console.log("I don't have enough money for a gumball");
+} else {
+  console.log("I have enough money for a gumball");
+}
 
 /*
 YOU DO:
@@ -82,15 +89,24 @@ When cupsOfFlour = 2 and hasSauce = true, your program should log "I can make pi
 When cupsOfFlour = 3 and hasSauce = true, your program should log "I can make pizza";
 */
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 2;
 var hasSauce = true;
 
-// Write your conditional here
+if (cupsOfFlour >= 2 && hasSauce) {
+  console.log("I can make pizza");
+} else {
+  console.log("I cannot make pizza");
+}
+
 
 
 /*
-For the last two exercises, an ideal solution probably uses a logical operator. 
+For the last two exercises, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
 
+I used a logical operator in the pizza exercise which more natural to do.
+
+I didn't see a way to use a logical operator in the quarter question that would make it more concise...
+That doesn't mean there isn't one though!
 */
