507f39d86aaea78c2d4eac4b3fac19ddef0891f9
Jake Dunafon <77365791+J-Dunny@users.noreply.github.com>
2021-10-27 15:53:03 -0700

    Update concatenation.js

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,7 +22,7 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:  String since the concatenation is in quotes.
 
 
 // YOU DO:
@@ -30,6 +30,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
+console.log("The " + speedy + " jumped over the " + slowPoke)
 
 
 // YOU DO:
@@ -39,6 +40,7 @@ var slowPoke = "lazy brown dog";
 
 slowPoke = "tortoise";
 speedy = "hare";
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!")
 
 
 // YOU DO:
@@ -46,10 +48,15 @@ speedy = "hare";
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
 
+var feeling = "love"
+var action = "eating"
+var food = "lasagna"
+console.log("I " + feeling + " " + action + " " + food + "!")
+
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
 // Did you run this file in your terminal to make sure everything printed out to the console 
-  // as you would expect? You should be doing this by now, and getting into the habit of it.
+  // as you would expect? You should be doing this by now, and getting into the habit of it. Yes
